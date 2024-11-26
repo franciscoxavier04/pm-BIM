a115cc50fca7b59121b45e379941f617e5bdd210
Dombi Attila <83396+dombesz@users.noreply.github.com>
2024-11-26 20:34:43 +0200

    Allow instantiation of Project::LifeCycleStepDefinition in order to let acts_as_list function correctly.

--- app/models/project/life_cycle_step_definition.rb
+++ app/models/project/life_cycle_step_definition.rb
@@ -42,15 +42,20 @@ class Project::LifeCycleStepDefinition < ApplicationRecord
 
   acts_as_list
 
-  def initialize(*args)
-    if instance_of? Project::LifeCycleStepDefinition
-      # Do not allow directly instantiating this class
-      raise NotImplementedError, "Cannot instantiate the base Project::LifeCycleStepDefinition class directly. " \
-                                 "Use Project::StageDefinition or Project::GateDefinition instead."
-    end
-
-    super
-  end
+  # TODO: Enabling this causes an error in the acts_as_customizable gem
+  # /gems/acts_as_list-1.2.4/lib/acts_as_list/active_record/acts/position_column_method_definer.rb
+  # define_singleton_method :touch_record_sql do
+  #   new.touch_record_sql
+  # end
+  # Calling new will fail with the error below:
+  # def initialize(*args)
+  #   if instance_of? Project::LifeCycleStepDefinition
+  #     # Do not allow directly instantiating this class
+  #     raise NotImplementedError, "Cannot instantiate the base Project::LifeCycleStepDefinition class directly. " \
+  #                                "Use Project::StageDefinition or Project::GateDefinition instead."
+  #   end
+  #   super
+  # end
 
   def step_class
     raise NotImplementedError
