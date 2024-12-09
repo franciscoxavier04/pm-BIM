8b9059c0fcc342d53747cd85a7a21143ebecfb5b
ulferts <jens.ulferts@googlemail.com>
2024-12-09 13:04:01 +0100

    rely on default naming of index

--- db/migrate/20241125161226_unique_index_on_project_life_cycle_steps.rb
+++ db/migrate/20241125161226_unique_index_on_project_life_cycle_steps.rb
@@ -2,7 +2,6 @@ class UniqueIndexOnProjectLifeCycleSteps < ActiveRecord::Migration[7.1]
   def change
     add_index :project_life_cycle_steps,
               %i[project_id definition_id],
-              unique: true,
-              name: "index_project_life_cycle_steps_on_project_id_and_definition_id"
+              unique: true
   end
 end
