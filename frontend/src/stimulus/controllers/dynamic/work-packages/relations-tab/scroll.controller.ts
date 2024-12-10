2c3485312b2c997e6eff82483d7ba11b44d031f8
Behrokh Satarnejad <b.satarnejad@openproject.com>
2024-12-10 17:22:40 +0100

    remove settimeout from the controller and use requestAnimationFrame instead

--- frontend/src/stimulus/controllers/dynamic/work-packages/relations-tab/scroll.controller.ts
+++ frontend/src/stimulus/controllers/dynamic/work-packages/relations-tab/scroll.controller.ts
@@ -1,18 +1,17 @@
 import { Controller } from '@hotwired/stimulus';
 
 export default class ScrollController extends Controller {
-  static values = { targetId: String };
-  declare targetIdValue:string;
-
   connect() {
-    // Wait to ensure DOM is fully loaded
-    setTimeout(() => {
-      if (this.targetIdValue) {
-         const element = document.querySelector(`[data-test-selector='op-relation-row-${this.targetIdValue}']`) as HTMLElement;
-        if (element) {
-          element.scrollIntoView({ behavior: 'smooth', block: 'center' });
-        }
-      }
-    });
+    this.waitForRenderAndAct();
+  }
+
+  waitForRenderAndAct() {
+    const element = document.querySelector('[data-scroll-active="true"]');
+
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'center' });
+    } else {
+      requestAnimationFrame(this.waitForRenderAndAct.bind(this));
+    }
   }
 }
