9227339b6c232a36e8aff2648ca2808a23f08cbc
Eric Schubert <e.schubert@openproject.com>
2024-12-16 10:43:45 +0100

    [#59998] add feature flag for work package subject configuration
    - https://community.openproject.org/work_packages/59998
    - do not show tab "subject configuration" if feature flag is not active

--- config/initializers/feature_decisions.rb
+++ config/initializers/feature_decisions.rb
@@ -50,6 +50,10 @@ OpenProject::FeatureDecisions.add :generate_pdf_from_work_package,
 OpenProject::FeatureDecisions.add :recurring_meetings,
                                   description: "Differentiate between one-time and recurring meetings."
 
+OpenProject::FeatureDecisions.add :generate_work_package_subjects,
+                                  description: "Allows the configuration for work package types to have " \
+                                               "automatically generated work package subjects."
+
 # TODO: Remove once the feature flag primerized_work_package_activities is removed altogether
 OpenProject::FeatureDecisions.define_singleton_method(:primerized_work_package_activities_active?) do
   Rails.env.production? ||
