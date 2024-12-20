2358f56cbf8eed26cd607ab8dec5aad7c00d841a
Oliver Günther <mail@oliverguenther.de>
2024-12-20 17:18:34 +0100

    Restore setting the first part of title if it contains the full base

--- frontend/src/app/core/html/op-title.service.ts
+++ frontend/src/app/core/html/op-title.service.ts
@@ -8,13 +8,27 @@ export class OpTitleService {
   constructor(private titleService:Title) {
   }
 
+  public get current():string {
+    return this.titleService.getTitle();
+  }
+
   public get base():string {
     const appTitle = document.querySelector('meta[name=app_title]') as HTMLMetaElement;
     return appTitle.content;
   }
 
+  public get titleParts():string[] {
+    return this.current.split(titlePartsSeparator);
+  }
+
   public setFirstPart(value:string) {
-    const newTitle = [value, this.base].join(titlePartsSeparator);
-    this.titleService.setTitle(newTitle);
+    if (this.current.includes(this.base) && this.current.includes(titlePartsSeparator)) {
+      const parts = this.titleParts;
+      parts[0] = value;
+      this.titleService.setTitle(parts.join(titlePartsSeparator));
+    } else {
+      const newTitle = [value, this.base].join(titlePartsSeparator);
+      this.titleService.setTitle(newTitle);
+    }
   }
 }
