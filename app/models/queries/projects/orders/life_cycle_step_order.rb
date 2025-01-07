a7f7e9fb9d2082221487219fc57fcc7b4bbb1dbf
Tobias Dillmann <github@pentokami.de>
2025-01-07 09:58:02 +0100

    [#59184] simplify by using subqueries in favor of CTE
    It is supposed to be a bit more performant for a case like this, too.

--- app/models/queries/projects/orders/life_cycle_step_order.rb
+++ app/models/queries/projects/orders/life_cycle_step_order.rb
@@ -55,22 +55,25 @@ class Queries::Projects::Orders::LifeCycleStepOrder < Queries::Orders::Base
 
   def joins
     <<~SQL.squish
-      LEFT JOIN #{cte_name} ON #{cte_name}.project_id = projects.id
+      LEFT JOIN (
+              SELECT steps.*, def.name, def.id as def_id
+              FROM project_life_cycle_steps steps
+              LEFT JOIN project_life_cycle_step_definitions def
+                ON steps.definition_id = def.id
+              WHERE
+                steps.active = true
+                AND def.id = #{life_cycle_step_definition.id}
+            ) #{subquery_table_name} ON #{subquery_table_name}.project_id = projects.id
     SQL
   end
 
-  def cte_name
+  def subquery_table_name
     # Since we can combine multiple queries with their respective ORDER BY clauses, we need to make sure
-    # that the name of our CTE is unique. To achieve this, we use the definition_id as part of the CTE name.
+    # that the name of our tables is unique. It suffices to include the definition id into the name as there can only
+    # ever be one order statement per definition.
     definition_id = life_cycle_step_definition.id
 
     :"life_cycle_steps_cte_#{definition_id}"
-  end
-
-  def cte_statement
-    Project::LifeCycleStep.select("*, def.id as def_id")
-                          .joins("LEFT JOIN project_life_cycle_step_definitions def ON definition_id = def.id")
-                          .where(active: true, definition_id: life_cycle_step_definition.id)
   end
 
   def order(scope)
@@ -83,7 +86,7 @@ class Queries::Projects::Orders::LifeCycleStepOrder < Queries::Orders::Base
   def order_condition
     # To avoid SQL injection warnings, we use Arel to build the condition.
     # Note that this SQL query uses the CTE defined in `cte_statement`.
-    steps_cte = Arel::Table.new(cte_name.to_s)
+    steps_cte = Arel::Table.new(subquery_table_name.to_s)
 
     # WHERE cte_name.def_id = life_cycle_step_definition.id OR cte_name.def_id IS NULL
     steps_cte[:def_id]
@@ -92,7 +95,7 @@ class Queries::Projects::Orders::LifeCycleStepOrder < Queries::Orders::Base
   end
 
   def order_by_start_and_end_date
-    steps_table = Arel::Table.new(cte_name.to_s)
+    steps_table = Arel::Table.new(subquery_table_name.to_s)
 
     # Even though a gate does not define an end_date, this code still works.
     [
