0e77873f52fd0db2008339305d407991d0a085c0
Kabiru Mwenja <k.mwenja@openproject.com>
2025-01-10 14:19:04 +0300

    Go all the way with hardcoded values

--- db/migrate/20250108100511_remove_incorrect_manage_own_reminders_permission.rb
+++ db/migrate/20250108100511_remove_incorrect_manage_own_reminders_permission.rb
@@ -1,10 +1,14 @@
 class RemoveIncorrectManageOwnRemindersPermission < ActiveRecord::Migration[7.1]
   def up
     # Remove manage_own_reminders permission from non member and anonymous roles
+    # Use hardcoded values for `Role::BUILTIN_NON_MEMBER` and `Role::BUILTIN_ANONYMOUS`
+    # to avoid breaking the migration if the values are changed in the future
+    non_member_builtin = 1 # Role::BUILTIN_NON_MEMBER
+    anonymous_builtin = 2 # Role::BUILTIN_ANONYMOUS
     execute <<-SQL.squish
       DELETE FROM role_permissions
       WHERE role_id IN (
-        SELECT id FROM roles WHERE builtin IN (#{Role::BUILTIN_NON_MEMBER}, #{Role::BUILTIN_ANONYMOUS})
+        SELECT id FROM roles WHERE builtin IN (#{non_member_builtin}, #{anonymous_builtin})
       )
       AND permission = 'manage_own_reminders'
     SQL
