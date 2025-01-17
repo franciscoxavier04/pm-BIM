71f99d273893dca33f2cf91a65ad00ebe3199398
Eric Schubert <e.schubert@openproject.com>
2025-01-17 14:26:29 +0100

    [#60518] validate enterprise action in backend
    - https://community.openproject.org/work_packages/60518
    - if UI is tricked, the update is guarded by an enterprise action,
      prevent persistence
    - added unit tests for pattern validation

--- spec/controllers/work_packages/types/subject_configuration_controller_spec.rb
+++ spec/controllers/work_packages/types/subject_configuration_controller_spec.rb
@@ -73,7 +73,7 @@ RSpec.describe WorkPackages::Types::SubjectConfigurationController do
 
     context "if form data is invalid" do
       let(:form_data) { { subject_configuration: "generated", pattern: nil } }
-      let(:expected_pattern_data) { { "subject" => { blueprint: "", enabled: true } } }
+      let(:expected_pattern_data) { { subject: { blueprint: "", enabled: true } } }
       let(:service_result) { ServiceResult.failure }
 
       it "renders the edit template" do
@@ -85,7 +85,7 @@ RSpec.describe WorkPackages::Types::SubjectConfigurationController do
     context "if form data is valid" do
       context "with generated subject configuration" do
         let(:form_data) { { subject_configuration: "generated", pattern: "Vacation - {{assignee}}" } }
-        let(:expected_pattern_data) { { "subject" => { blueprint: "Vacation - {{assignee}}", enabled: true } } }
+        let(:expected_pattern_data) { { subject: { blueprint: "Vacation - {{assignee}}", enabled: true } } }
         let(:service_result) { ServiceResult.success }
 
         it "redirects to the current tab path" do
@@ -95,7 +95,7 @@ RSpec.describe WorkPackages::Types::SubjectConfigurationController do
 
       context "with manual subject configuration, but still persisted blueprint" do
         let(:form_data) { { subject_configuration: "manual", pattern: "Vacation - {{assignee}}" } }
-        let(:expected_pattern_data) { { "subject" => { blueprint: "Vacation - {{assignee}}", enabled: false } } }
+        let(:expected_pattern_data) { { subject: { blueprint: "Vacation - {{assignee}}", enabled: false } } }
         let(:service_result) { ServiceResult.success }
 
         it "redirects to the current tab path" do
