dca3349fc59ff66fe55c7d937435c0f85b802e62
Christophe Bliard <c.bliard@openproject.com>
2025-01-24 11:13:12 +0100

    Fix flaky spec
    Flaky spec was `./modules/costs/spec/features/cost_entries/add_cost_entry_spec.rb:114`
    It was failing for instance in https://github.com/opf/openproject/actions/runs/12934263465/job/36078482478
    
    The flakiness is due to the js on the page not working until the
    spent_on date field is fully loaded. When the unit cost values are
    input, when the js is not working, then the computed "Costs" value is
    not updated, and so the test fails.
    
    The fix is simple: wait for the spent_on date field to be loaded with
    `page.has_field?(CostEntry.human_attribute_name(:spent_on))`.
    
    Then I refactored things a bit to remove some duplicate code and use
    some accessible selectors instead of css selectors.

--- spec/support/pages/work_packages/full_work_package.rb
+++ spec/support/pages/work_packages/full_work_package.rb
@@ -87,6 +87,13 @@ module Pages
       end
     end
 
+    def select_log_unit_costs_action
+      SeleniumHubWaiter.wait
+      click_button(I18n.t("js.button_more"))
+      find(:menuitem, text: I18n.t(:button_log_costs)).click
+      Pages::WorkPackages::CostEntries.new.wait_for_spent_on_date_field_to_be_loaded
+    end
+
     def wait_for_activity_tab
       expect(page).to have_test_selector("op-wp-activity-tab", wait: 10)
       # wait for stimulus js component to be mounted
