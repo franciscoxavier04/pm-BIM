a18028f72f73b146eabba7cb285dbe2e56a7bbf2
Oliver Günther <mail@oliverguenther.de>
2025-01-29 09:19:51 +0100

    Seed weekly as recurring meeting
    https://community.openproject.org/work_packages/60135

--- modules/meeting/app/seeders/meetings/demo_data/meeting_series_seeder.rb
+++ modules/meeting/app/seeders/meetings/demo_data/meeting_series_seeder.rb
@@ -28,9 +28,9 @@
 
 module Meetings
   module DemoData
-    class MeetingSeeder < ::BasicData::ModelSeeder
-      self.model_class = StructuredMeeting
-      self.seed_data_model_key = "meetings"
+    class MeetingSeriesSeeder < ::BasicData::ModelSeeder
+      self.model_class = RecurringMeeting
+      self.seed_data_model_key = "meeting_series"
 
       attr_reader :project
 
@@ -39,13 +39,41 @@ module Meetings
         @project = project
       end
 
+      def prepare!
+        project.enabled_modules << EnabledModule.new(name: "meetings")
+      end
+
+      def create_model!(model_data)
+        series = super
+        create_meeting_template!(series, model_data)
+        RecurringMeetings::InitNextOccurrenceJob.perform_later(series, series.first_occurrence)
+      end
+
+      def create_meeting_template!(series, model_data)
+        template = StructuredMeeting.new(template_attributes(model_data))
+        template.template = true
+        template.recurring_meeting = series
+
+        template.save!
+        reference = :"#{model_data['reference']}_template"
+        seed_data.store_reference(reference, template)
+      end
+
       def model_attributes(meeting_data)
         {
           title: meeting_data["title"],
           author: seed_data.find_reference(meeting_data["author"]),
           duration: minutes_to_hours(meeting_data["duration"]),
+          start_time: Time.current.next_weekday + 10.hours,
+          frequency: meeting_data["frequency"],
+          interval: meeting_data["interval"],
           project:
         }
+      end
+
+      def template_attributes(meeting_data)
+        model_attributes(meeting_data)
+          .slice(:title, :author, :duration, :start_time, :project)
       end
 
       def minutes_to_hours(duration)
