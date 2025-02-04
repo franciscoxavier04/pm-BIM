80922469a97c41d33fb118f70f263374aa312ead
Ivan Kuchin <i.kuchin@openproject.com>
2025-02-04 12:22:30 +0100

    fix and simplify some requires in specs

--- spec/lib/journal_formatter/ignore_non_working_days_spec.rb
+++ spec/lib/journal_formatter/ignore_non_working_days_spec.rb
@@ -26,7 +26,7 @@
 # See COPYRIGHT and LICENSE files for more details.
 #++
 
-require File.expand_path("#{File.dirname(__FILE__)}/../../spec_helper.rb")
+require "spec_helper"
 
 RSpec.describe OpenProject::JournalFormatter::IgnoreNonWorkingDays do
   let(:klass) { described_class }
