997ed8576c34ab568a208a69de6b934d08419763
Oliver Günther <mail@oliverguenther.de>
2025-02-04 11:27:09 +0100

    Scrub turbo scripts before rendering body

--- frontend/src/turbo/turbo-event-listeners.ts
+++ frontend/src/turbo/turbo-event-listeners.ts
@@ -1,3 +1,6 @@
+import { TurboHelpers } from './helpers';
+import { StreamElement } from '@hotwired/turbo';
+
 export function addTurboEventListeners() {
   // Close the primer dialog when the form inside has been submitted with a success response.
   //
@@ -24,5 +27,24 @@ export function addTurboEventListeners() {
     const headers = event.detail.fetchOptions.headers as Record<string, string>;
     headers['Turbo-Referrer'] = window.location.href;
     headers['X-Turbo-Nonce'] = document.getElementsByName('csp-nonce')[0]?.getAttribute('content') || '';
+  });
+
+  // Turbo adds nonces to all scripts, even though we want to explicitly pass nonces
+  // https://github.com/hotwired/turbo/issues/294#issuecomment-2633216052
+  // We remove them manually as a workaround
+  // in Handle Turbo Drive page loads (full reloads)
+  document.addEventListener('turbo:before-render', (event) => {
+    TurboHelpers.scrubScriptElements(event.detail.newBody);
+  });
+
+  // in Turbo Streams (partial updates)
+  document.addEventListener('turbo:before-stream-render', (event) => {
+    const content = (event.target as StreamElement).templateContent;
+    TurboHelpers.scrubScriptElements(content);
+  });
+
+  // in Turbo Frames (when they load new content)
+  document.addEventListener('turbo:frame-render', (event) => {
+    TurboHelpers.scrubScriptElements(event.target as HTMLElement);
   });
 }
