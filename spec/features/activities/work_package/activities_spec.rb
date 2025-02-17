42428720c1b32ed2e382ce12ffbf1636d45043d0
Kabiru Mwenja <k.mwenja@openproject.com>
2025-02-17 13:13:05 +0300

    Remove static wait time on activities filter journal
    Reinstate DOM enabled wait time introduced in https://github.com/opf/openproject/pull/17055

--- spec/features/activities/work_package/activities_spec.rb
+++ spec/features/activities/work_package/activities_spec.rb
@@ -562,7 +562,6 @@ RSpec.describe "Work package activity", :js, :with_cuprite do
 
       it "resets an only_changes filter if a comment is added by the user", :aggregate_failures do
         activity_tab.filter_journals(:only_changes)
-        wait_for_filter
 
         # expect only the changes
         activity_tab.expect_no_journal_notes(text: "First comment by admin")
@@ -570,13 +569,10 @@ RSpec.describe "Work package activity", :js, :with_cuprite do
 
         # add a comment
         activity_tab.add_comment(text: "Third comment by admin")
-        wait_for_filter
 
         # the only_changes filter should be reset
         activity_tab.expect_journal_notes(text: "Third comment by admin")
       end
-
-      def wait_for_filter = sleep(1)
     end
   end
 
