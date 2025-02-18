88a893633e864260f8b24ef9b0745beb9fd43c10
Kabiru Mwenja <k.mwenja@openproject.com>
2025-02-18 14:23:26 +0300

    switch `wp_page.wait_for_activity_tab` to rspec wait

--- spec/support/pages/work_packages/abstract_work_package.rb
+++ spec/support/pages/work_packages/abstract_work_package.rb
@@ -80,9 +80,9 @@ module Pages
     end
 
     def wait_for_activity_tab
-      expect(page).to have_test_selector("op-wp-activity-tab", wait: 10)
-      # wait for stimulus js component to be mounted
-      expect(page).to have_css('[data-test-selector="op-wp-activity-tab"][data-stimulus-controller-connected="true"]')
+      wait_for { page }.to have_test_selector("op-wp-activity-tab")
+      # ensure stimulus controller is mounted
+      expect(page).to have_css('[data-stimulus-controller-connected="true"]')
     end
 
     def expect_any_active_inline_edit_field
