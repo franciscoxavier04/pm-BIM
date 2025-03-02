9911932e5168f080571a8a59967ce2d8ad976f89
ulferts <jens.ulferts@googlemail.com>
2025-03-02 12:21:56 +0100

    adapt active job specs

--- modules/meeting/spec/services/recurring_meetings/update_service_integration_spec.rb
+++ modules/meeting/spec/services/recurring_meetings/update_service_integration_spec.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 #-- copyright
 # OpenProject is an open source project management software.
 # Copyright (C) the OpenProject GmbH
@@ -97,13 +98,13 @@ RSpec.describe RecurringMeetings::UpdateService, "integration", type: :model do
       end
     end
 
-    context "when updating the frequency and start_time" do
+    context "when updating the frequency and start_time",
+            with_good_job: RecurringMeetings::InitNextOccurrenceJob do
       let(:params) do
         { start_time: Time.zone.today + 2.days + 11.hours }
       end
 
       before do
-        ActiveJob::Base.disable_test_adapter
         RecurringMeetings::InitNextOccurrenceJob
           .set(wait_until: Time.zone.today + 1.day + 10.hours)
           .perform_later(series)
