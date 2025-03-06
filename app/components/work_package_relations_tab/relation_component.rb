a60fba57a5f4233a40702318cfa0347f973e4046
Behrokh Satarnejad <b.satarnejad@openproject.com>
2025-03-06 16:53:16 +0100

    instead of check for showing the delete option, we extend the check method for showing action menu

--- app/components/work_package_relations_tab/relation_component.rb
+++ app/components/work_package_relations_tab/relation_component.rb
@@ -56,17 +56,12 @@ class WorkPackageRelationsTab::RelationComponent < ApplicationComponent
   end
 
   def allowed_to_manage_subtasks?
-    helpers.current_user.allowed_in_project?(:manage_subtasks, @work_package.project)
+    helpers.current_user.allowed_in_project?(:manage_subtasks, @work_package.project) &&
+      helpers.current_user.allowed_in_project?(:manage_subtasks, @child.project)
   end
 
   def allowed_to_manage_relations?
     helpers.current_user.allowed_in_project?(:manage_work_package_relations, @work_package.project)
-  end
-
-  def should_render_delete_option?
-    return true unless parent_child_relationship?
-
-    helpers.current_user.allowed_in_project?(:manage_subtasks, @child.project)
   end
 
   def visible?
