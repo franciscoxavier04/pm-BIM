5b36619c41c513621cc68be4ea4532631627ec53
Behrokh Satarnejad <b.satarnejad@openproject.com>
2025-03-07 13:12:43 +0100

    Change aria-label for today link if it is a milestone wp

--- app/components/work_packages/date_picker/date_form_component.rb
+++ app/components/work_packages/date_picker/date_form_component.rb
@@ -100,7 +100,7 @@ module WorkPackages
         render(
           Primer::Beta::Link.new(
             href: "",
-            "aria-label": I18n.t("label_today_as_#{name}"),
+            "aria-label": @is_milestone ? I18n.t("label_today_as_date") : I18n.t("label_today_as_#{name}"),
             data: {
               action: "work-packages--date-picker--preview#setTodayForField",
               "work-packages--date-picker--preview-field-reference-param": "work_package_#{name}",
