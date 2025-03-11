7789b8545c1b053e36f939f6cf5f783f1ab9b7f0
Jan Sandbrink <j.sandbrink@openproject.com>
2025-03-11 10:40:44 +0100

    Allow access to new file links by creator
    For example on the work package create UI, it
    is already possible to link files, even though the
    WP does not yet exist. In this case it's a usability
    issue if the UI elements do not yet work as they should.
    
    As long as there is no associated container, it should be no
    issue to assume that the creator has all the necessary permissions
    to view a file link. Downloading the associated file is still
    limited by permissions on the storage, so this should not
    be abusable to retain long-term access to a file.

--- modules/storages/spec/requests/api/v3/file_links/file_links_api_spec.rb
+++ modules/storages/spec/requests/api/v3/file_links/file_links_api_spec.rb
@@ -518,7 +518,17 @@ RSpec.describe "API v3 file links resource" do
     context "if file link does not have a container." do
       let(:file_link) { create(:file_link) }
 
-      it_behaves_like "not found"
+      context "and the current user is the creator of the link" do
+        let(:current_user) { file_link.creator }
+
+        it "is successful" do
+          expect(subject.status).to be 200
+        end
+      end
+
+      context "and the current user is not the creator of the link" do
+        it_behaves_like "not found"
+      end
     end
   end
 
