7cd2001fc78de6a0984909b0e726fb20c87bc65b
Kabiru Mwenja <k.mwenja@openproject.com>
2025-03-11 21:10:27 +0300

    Revert "Simplify highlight toggle via `classList.toggle()`"
    This reverts commit 3213e2561d51ba9d95412c2410eeff0b12a75b05.
    
    The input checked status is actually needed

--- frontend/src/stimulus/controllers/dynamic/work-packages/activities-tab/restricted-comment.controller.ts
+++ frontend/src/stimulus/controllers/dynamic/work-packages/activities-tab/restricted-comment.controller.ts
@@ -43,6 +43,10 @@ export default class RestrictedCommentController extends Controller {
   }
 
   toggleBackgroundColor():void {
-    this.formContainerTarget.classList.toggle(this.highlightClass);
+    if (this.restrictedCheckboxTarget.checked) {
+      this.formContainerTarget.classList.add(this.highlightClass);
+    } else {
+      this.formContainerTarget.classList.remove(this.highlightClass);
+    }
   }
 }
