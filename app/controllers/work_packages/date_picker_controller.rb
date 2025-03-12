0d18d08651a2c1cc9f67b7964bc2e1481500a2c3
Christophe Bliard <c.bliard@openproject.com>
2025-03-12 17:13:05 +0100

    [62012] Avoid resetting calendar when enabling a date field
    https://community.openproject.org/wp/62012
    
    When clicking on a date field in single date mode, it used to
    reinitialize the flatpickr instance. This happens because it was using a
    turbo-stream to update the dialog. When doing so, the node where the
    flatpickr is attached is replaced.
    
    We have some logic to avoid this replacement in the preview controller:
    it listens to turbo:before-frame-render events and avoids morphing for
    tags starting with "opce".
    
    As the logic is already there, this commit uses a turbo-frame instead of
    a turbo-stream. This way the flatpickr instance is not replaced and the
    calendar view is kept.
    
    The controller had to be updated to alter the work package attributes
    with the given params.

--- app/controllers/work_packages/date_picker_controller.rb
+++ app/controllers/work_packages/date_picker_controller.rb
@@ -43,6 +43,8 @@ class WorkPackages::DatePickerController < ApplicationController
   attr_accessor :work_package
 
   def show
+    set_date_attributes_to_work_package
+
     respond_to do |format|
       format.html do
         render :show,
@@ -51,8 +53,6 @@ class WorkPackages::DatePickerController < ApplicationController
       end
 
       format.turbo_stream do
-        set_date_attributes_to_work_package
-
         replace_via_turbo_stream(
           component: datepicker_modal_component
         )
