0d25e8370c3603e18e2ea1cb3afac0261042b6a8
Tobias Dillmann <github@pentokami.de>
2025-03-12 16:42:07 +0100

    [#36559] minor code style fixes

--- modules/xls_export/spec/lib/custom_field_xls_export_spec.rb
+++ modules/xls_export/spec/lib/custom_field_xls_export_spec.rb
@@ -1,3 +1,33 @@
+# frozen_string_literal: true
+
+#-- copyright
+# OpenProject is an open source project management software.
+# Copyright (C) the OpenProject GmbH
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License version 3.
+#
+# OpenProject is a fork of ChiliProject, which is a fork of Redmine. The copyright follows:
+# Copyright (C) 2006-2013 Jean-Philippe Lang
+# Copyright (C) 2010-2013 the ChiliProject Team
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+# See COPYRIGHT and LICENSE files for more details.
+#++
+
 require "spec_helper"
 require "spreadsheet"
 
@@ -12,7 +42,7 @@ RSpec.describe "WorkPackageXlsExport Custom Fields" do
       multi_value: true,
       types: [type],
       projects: [project],
-      possible_values: ["ham", "onions", "pineapple", "mushrooms"]
+      possible_values: %w[ham onions pineapple mushrooms]
     )
   end
 
@@ -68,7 +98,7 @@ RSpec.describe "WorkPackageXlsExport Custom Fields" do
 
   it "produces the valid XLS result" do
     expect(query.columns.map(&:name)).to eq [:subject, custom_field.column_name.to_sym]
-    expect(sheet.rows.first.take(2)).to eq ["Subject", "Ingredients"]
+    expect(sheet.rows.first.take(2)).to eq %w[Subject Ingredients]
 
     # Subjects
     expect(sheet.row(1)[0]).to eq(work_package1.subject)
