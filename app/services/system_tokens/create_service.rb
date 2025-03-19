096c07e2dd8339f85e727b892971f7ccb2f7c610
Jan Sandbrink <j.sandbrink@openproject.com>
2025-03-19 08:59:11 +0100

    WIP

---
+++ app/services/system_tokens/create_service.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+#-- copyright
+# OpenProject is an open source project management software.
+# Copyright (C) the OpenProject GmbH
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License version 3.
+#
+# OpenProject is a fork of ChiliProject, which is a fork of Redmine. The copyright follows:
+# Copyright (C) 2006-2013 Jean-Philippe Lang
+# Copyright (C) 2010-2013 the ChiliProject Team
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+# See COPYRIGHT and LICENSE files for more details.
+#++
+
+module SystemTokens
+  class CreateService
+    def call(scopes:, expires_in:)
+      # TODO: TokenName model that is associated to the token and allows assigning a user-provided
+      #       label to the token
+      # TODO: confirm that these tokens can be validated in API requests
+      Doorkeeper::OAuth::ClientCredentials::Creator.new.call(
+        Doorkeeper::Application.system_tokens_instance,
+        scopes,
+        use_refresh_token: false,
+        expires_in:
+      )
+    end
+  end
+end
