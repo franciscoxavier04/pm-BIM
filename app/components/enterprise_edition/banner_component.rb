4ff01e16d82f0db4427a6aa963fd60dbe115ed00
Oliver Günther <mail@oliverguenther.de>
2025-03-25 13:58:56 +0100

    Type form banner

--- app/components/enterprise_edition/banner_component.rb
+++ app/components/enterprise_edition/banner_component.rb
@@ -76,18 +76,26 @@ module EnterpriseEdition
 
     def description
       @description || begin
-        I18n.t("ee.upsale.#{feature_key}.description")
-      rescue StandardError
-        I18n.t("ee.upsale.#{feature_key}.description_html")
+        if I18n.exists?(:"ee.upsale.#{feature_key}.description_html")
+          I18n.t("ee.upsale.#{feature_key}.description_html").html_safe
+        else
+          I18n.t("ee.upsale.#{feature_key}.description")
+        end
       end
     rescue I18n::MissingTranslationData => e
       raise e.exception(
         <<~TEXT.squish
-          The expected '#{I18n.locale}.ee.upsale.#{feature_key}.description' key does not exist.
+          The expected '#{I18n.locale}.ee.upsale.#{feature_key}.description' nor '#{I18n.locale}.ee.upsale.#{feature_key}.description_html' key does not exist.
           Ideally, provide it in the locale file.
           If that isn't applicable, a description parameter needs to be provided.
         TEXT
       )
+    end
+
+    def features
+      return @features if defined?(@features)
+
+      @features = I18n.t("ee.upsale.#{feature_key}.features", default: nil)&.values
     end
 
     def plan
