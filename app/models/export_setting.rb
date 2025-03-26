bb98ff47e9623f8a79a77f5501db9e754268011e
Tobias Dillmann <github@pentokami.de>
2025-03-26 16:45:02 +0100

    [#57388] create table: export_settings

---
+++ app/models/export_setting.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+#-- copyright
+# OpenProject is an open source project management software.
+# Copyright (C) the OpenProject GmbH
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License version 3.
+#
+# OpenProject is a fork of ChiliProject, which is a fork of Redmine. The copyright follows:
+# Copyright (C) 2006-2013 Jean-Philippe Lang
+# Copyright (C) 2010-2013 the ChiliProject Team
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+# See COPYRIGHT and LICENSE files for more details.
+#++
+
+class ExportSetting < ApplicationRecord
+  belongs_to :query
+
+  POSSIBLE_FORMATS = %w[csv xls pdf_table pdf_report pdf_gantt].freeze
+
+  validates :query_id, presence: true
+  validates :format, presence: true, inclusion: { in: POSSIBLE_FORMATS }
+  validates :settings, presence: true
+
+  validate :unique_format_per_query
+
+  private
+
+  def unique_format_per_query
+    if ExportSetting.exists?(query_id: query_id, format: format) && (new_record? || format_changed?)
+      errors.add(:format, "there already is an export setting for this query with this format")
+    end
+  end
+end
