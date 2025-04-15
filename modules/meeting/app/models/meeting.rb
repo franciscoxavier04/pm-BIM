Dileep9999/openproject

Add backlog component and visibility conditions

modules/meeting/app/models/meeting.rb
@@ -50,8 +50,8 @@ class Meeting < ApplicationRecord
            class_name: "MeetingParticipant",
            after_add: :send_participant_added_mail
 
-  has_many :sections, dependent: :destroy, class_name: "MeetingSection"
-  has_one :backlog, -> { where(backlog: true) }, class_name: "MeetingSection"
+  has_many :sections, -> { where(backlog: false) }, dependent: :destroy, class_name: "MeetingSection"
+  has_one :backlog, -> { where(backlog: true) }, dependent: :destroy, class_name: "MeetingSection"
   has_many :agenda_items, dependent: :destroy, class_name: "MeetingAgendaItem"
 
   scope :templated, -> { where(template: true) }

franciscoxavier04/pm-BIM

modules/meeting/app/models/meeting.rb
@@ -53,8 +53,8 @@ class Meeting < ApplicationRecord
            after_add: :send_participant_added_mail
 
   has_many :agenda_items, dependent: :destroy, class_name: "MeetingAgendaItem", inverse_of: :meeting
-  has_many :sections, dependent: :delete_all, class_name: "MeetingSection"
-  has_one :backlog, -> { where(backlog: true) }, class_name: "MeetingSection"
+  has_many :sections, -> { where(backlog: false) }, dependent: :delete_all, class_name: "MeetingSection"
+  has_one :backlog, -> { where(backlog: true) }, dependent: :destroy, class_name: "MeetingSection"
 
   accepts_nested_attributes_for :agenda_items
 