Ericaax0/openproject

Rework "open storage" action
The previous implementation was a deeply nested decision tree for
different error cases or other unexpected states.

Most of this was necessary, because setting up correct permissions is partially
happening asynchronously. Some of the redirects would eventually have rendered a modal
that would periodically check whether user permissions appeared in the background.

All of this was replaced by an approach where we immediately (synchronously) ensure
that the setup is correct.

modules/storages/app/services/storages/one_drive_managed_folder_create_service.rb
@@ -29,35 +29,31 @@
 #++
 
 module Storages
-  class OneDriveManagedFolderSyncService < BaseService
-    include Injector["one_drive.commands.create_folder", "one_drive.commands.rename_file",
-                     "one_drive.commands.set_permissions", "one_drive.queries.files", "one_drive.authentication.userless"]
-
+  class OneDriveManagedFolderCreateService < BaseService
     using Peripherals::ServiceResultRefinements
 
-    OP_PERMISSIONS = %i[read_files write_files create_files delete_files share_files].freeze
+    def self.i18n_key = "one_drive_sync_service"
 
-    def self.i18n_key = "OneDriveSyncService"
-
-    def self.call(storage)
-      new(storage).call
+    class << self
+      def call(storage:, project_storages: nil)
+        new(storage:, project_storages:).call
+      end
     end
 
-    def initialize(storage, **)
-      super(**)
+    def initialize(storage:, project_storages: nil)
+      super()
       @storage = storage
-      @result = ServiceResult.success(errors: ActiveModel::Errors.new(self))
+      @hide_missing_folders = project_storages.nil?
+      @project_storages = project_storages || @storage.project_storages.active.automatic
     end
 
     def call
       with_tagged_logger([self.class.name, "storage-#{@storage.id}"]) do
-        return unless @storage.automatic_management_enabled?
-
-        info "Starting AMPF Sync for OneDrive Storage #{@storage.id}"
         existing_remote_folders = remote_folders_map(@storage.drive_id).on_failure { return @result }.result
 
-        ensure_folders_exist(existing_remote_folders).on_success { hide_inactive_folders(existing_remote_folders) }
-        apply_permission_to_folders
+        ensure_folders_exist(existing_remote_folders).on_success do
+          hide_inactive_folders(existing_remote_folders) if @hide_missing_folders
+        end
 
         @result
       end
@@ -65,38 +61,9 @@ module Storages
 
     private
 
-    # rubocop:disable Metrics/AbcSize
-    def apply_permission_to_folders
-      info "Setting permissions to project folders"
-      active_project_storages_scope.includes(:project)
-                                   .where.not(project_folder_id: nil)
-                                   .find_each do |project_storage|
-        permissions = admin_remote_identities_scope
-                        .pluck(:origin_user_id)
-                        .map do |origin_user_id|
-          { user_id: origin_user_id, permissions: [:write_files] }
-        end
-
-        project_remote_identities(project_storage).each do |identity|
-          add_user_to_permission_list(permissions, identity, project_storage.project)
-        end
-
-        info "Setting permissions for #{project_storage.managed_project_folder_name}: #{permissions}"
-
-        project_folder_id = project_storage.project_folder_id
-        build_permissions_input_data(project_folder_id, permissions)
-          .either(
-            ->(input_data) { set_permissions.call(storage: @storage, auth_strategy:, input_data:) },
-            ->(failure) { log_validation_error(failure, project_folder_id:, permissions:) }
-          )
-      end
-    end
-
-    # rubocop:enable Metrics/AbcSize
-
     def ensure_folders_exist(folder_map)
       info "Ensuring that automatically managed project folders exist and are correctly named."
-      active_project_storages_scope.includes(:project).find_each do |project_storage|
+      @project_storages.includes(:project).find_each do |project_storage|
         unless folder_map.key?(project_storage.project_folder_id)
           info "#{project_storage.managed_project_folder_path} does not exist. Creating..."
           next create_remote_folder(project_storage.managed_project_folder_path, project_storage.id)
@@ -131,17 +98,7 @@ module Storages
     end
 
     def inactive_folder_ids(folder_map)
-      folder_map.keys - active_project_storages_scope.pluck(:project_folder_id).compact
-    end
-
-    def add_user_to_permission_list(permissions, identity, project)
-      op_user_permissions = identity.user.all_permissions_for(project)
-
-      if op_user_permissions.member?(:write_files)
-        permissions << { user_id: identity.origin_user_id, permissions: [:write_files] }
-      elsif op_user_permissions.member?(:read_files)
-        permissions << { user_id: identity.origin_user_id, permissions: [:read_files] }
-      end
+      folder_map.keys - @project_storages.pluck(:project_folder_id).compact
     end
 
     def rename_project_folder(current_folder_name, project_storage)
@@ -208,48 +165,22 @@ module Storages
       folders
     end
 
-    def project_remote_identities(project_storage)
-      project_remote_identities = client_remote_identities_scope.where.not(id: admin_remote_identities_scope).order(:id)
-
-      if project_storage.project.public? && ProjectRole.non_member.permissions.intersect?(OP_PERMISSIONS)
-        project_remote_identities
-      else
-        project_remote_identities.where(user: project_storage.project.users)
-      end
-    end
-
-    def active_project_storages_scope
-      @storage.project_storages.active.automatic
-    end
-
-    def client_remote_identities_scope
-      RemoteIdentity.includes(:user).where(integration: @storage)
-    end
-
-    def admin_remote_identities_scope
-      client_remote_identities_scope.where(user: User.admin.active)
-    end
-
     def root_folder = Peripherals::ParentFolder.new("/")
+
+    def create_folder = Peripherals::Registry.resolve("one_drive.commands.create_folder")
+
+    def rename_file = Peripherals::Registry.resolve("one_drive.commands.rename_file")
+
+    def set_permissions = Peripherals::Registry.resolve("one_drive.commands.set_permissions")
+
+    def files = Peripherals::Registry.resolve("one_drive.queries.files")
+
+    def userless = Peripherals::Registry.resolve("one_drive.authentication.userless")
 
     def auth_strategy = userless.call
 
     def build_permissions_input_data(file_id, user_permissions)
       Peripherals::StorageInteraction::Inputs::SetPermissions.build(file_id:, user_permissions:)
-    end
-
-    # @param attribute [Symbol] attribute to which the error will be tied to
-    # @param storage_error [Storages::StorageError] an StorageError instance
-    # @param options [Hash{Symbol => Object}] optional extra parameters for the message generation
-    # @return ServiceResult
-    def add_error(attribute, storage_error, options: {})
-      case storage_error.code
-      when :error, :unauthorized
-        @result.errors.add(:base, storage_error.code, **options)
-      else
-        @result.errors.add(attribute, storage_error.code, **options)
-      end
-      @result
     end
   end
 end

modules/storages/app/services/storages/one_drive_managed_folder_permissions_service.rb
@@ -1,0 +1,128 @@
+# frozen_string_literal: true
+
+#-- copyright
+# OpenProject is an open source project management software.
+# Copyright (C) the OpenProject GmbH
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License version 3.
+#
+# OpenProject is a fork of ChiliProject, which is a fork of Redmine. The copyright follows:
+# Copyright (C) 2006-2013 Jean-Philippe Lang
+# Copyright (C) 2010-2013 the ChiliProject Team
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+# See COPYRIGHT and LICENSE files for more details.
+#++
+
+module Storages
+  class OneDriveManagedFolderPermissionsService < BaseService
+    using Peripherals::ServiceResultRefinements
+
+    OP_PERMISSIONS = %i[read_files write_files create_files delete_files share_files].freeze
+
+    def self.i18n_key = "one_drive_sync_service"
+
+    class << self
+      def call(storage:, project_storages: nil)
+        new(storage:, project_storages:).call
+      end
+    end
+
+    def initialize(storage:, project_storages: nil)
+      super()
+      @storage = storage
+      @project_storages = project_storages || @storage.project_storages.active.automatic
+    end
+
+    def call
+      with_tagged_logger([self.class.name, "storage-#{@storage.id}"]) do
+        apply_permission_to_folders
+
+        @result
+      end
+    end
+
+    private
+
+    # rubocop:disable Metrics/AbcSize
+    def apply_permission_to_folders
+      info "Setting permissions to project folders"
+      @project_storages.includes(:project)
+                                   .where.not(project_folder_id: nil)
+                                   .find_each do |project_storage|
+        permissions = admin_remote_identities_scope
+                        .pluck(:origin_user_id)
+                        .map do |origin_user_id|
+          { user_id: origin_user_id, permissions: [:write_files] }
+        end
+
+        project_remote_identities(project_storage).each do |identity|
+          add_user_to_permission_list(permissions, identity, project_storage.project)
+        end
+
+        info "Setting permissions for #{project_storage.managed_project_folder_name}: #{permissions}"
+
+        project_folder_id = project_storage.project_folder_id
+        build_permissions_input_data(project_folder_id, permissions)
+          .either(
+            ->(input_data) { set_permissions.call(storage: @storage, auth_strategy:, input_data:) },
+            ->(failure) { log_validation_error(failure, project_folder_id:, permissions:) }
+          )
+      end
+    end
+
+    # rubocop:enable Metrics/AbcSize
+
+    def add_user_to_permission_list(permissions, identity, project)
+      op_user_permissions = identity.user.all_permissions_for(project)
+
+      if op_user_permissions.member?(:write_files)
+        permissions << { user_id: identity.origin_user_id, permissions: [:write_files] }
+      elsif op_user_permissions.member?(:read_files)
+        permissions << { user_id: identity.origin_user_id, permissions: [:read_files] }
+      end
+    end
+
+    def project_remote_identities(project_storage)
+      project_remote_identities = client_remote_identities_scope.where.not(id: admin_remote_identities_scope).order(:id)
+
+      if project_storage.project.public? && ProjectRole.non_member.permissions.intersect?(OP_PERMISSIONS)
+        project_remote_identities
+      else
+        project_remote_identities.where(user: project_storage.project.users)
+      end
+    end
+
+    def client_remote_identities_scope
+      RemoteIdentity.includes(:user).where(integration: @storage)
+    end
+
+    def admin_remote_identities_scope
+      client_remote_identities_scope.where(user: User.admin.active)
+    end
+
+    def set_permissions = Peripherals::Registry.resolve("one_drive.commands.set_permissions")
+
+    def userless = Peripherals::Registry.resolve("one_drive.authentication.userless")
+
+    def auth_strategy = userless.call
+
+    def build_permissions_input_data(file_id, user_permissions)
+      Peripherals::StorageInteraction::Inputs::SetPermissions.build(file_id:, user_permissions:)
+    end
+  end
+end

franciscoxavier04/pm-BIM

modules/storages/app/services/storages/one_drive_managed_folder_create_service.rb
@@ -29,35 +29,31 @@
 #++
 
 module Storages
-  class OneDriveManagedFolderSyncService < BaseService
-    include Injector["one_drive.commands.create_folder", "one_drive.commands.rename_file",
-                     "one_drive.commands.set_permissions", "one_drive.queries.files", "one_drive.authentication.userless"]
-
+  class OneDriveManagedFolderCreateService < BaseService
     using Peripherals::ServiceResultRefinements
 
-    OP_PERMISSIONS = %i[read_files write_files create_files delete_files share_files].freeze
+    def self.i18n_key = "one_drive_sync_service"
 
-    def self.i18n_key = "OneDriveSyncService"
-
-    def self.call(storage)
-      new(storage).call
+    class << self
+      def call(storage:, project_storages_scope: nil)
+        new(storage:, project_storages_scope:).call
+      end
     end
 
-    def initialize(storage, **)
-      super(**)
+    def initialize(storage:, project_storages_scope: nil)
+      super()
       @storage = storage
-      @result = ServiceResult.success(errors: ActiveModel::Errors.new(self))
+      @hide_missing_folders = project_storages_scope.nil?
+      @project_storages = (project_storages_scope || @storage.project_storages).active.automatic
     end
 
     def call
       with_tagged_logger([self.class.name, "storage-#{@storage.id}"]) do
-        return unless @storage.automatic_management_enabled?
-
-        info "Starting AMPF Sync for OneDrive Storage #{@storage.id}"
         existing_remote_folders = remote_folders_map(@storage.drive_id).on_failure { return @result }.result
 
-        ensure_folders_exist(existing_remote_folders).on_success { hide_inactive_folders(existing_remote_folders) }
-        apply_permission_to_folders
+        ensure_folders_exist(existing_remote_folders).on_success do
+          hide_inactive_folders(existing_remote_folders) if @hide_missing_folders
+        end
 
         @result
       end
@@ -65,38 +61,9 @@ module Storages
 
     private
 
-    # rubocop:disable Metrics/AbcSize
-    def apply_permission_to_folders
-      info "Setting permissions to project folders"
-      active_project_storages_scope.includes(:project)
-                                   .where.not(project_folder_id: nil)
-                                   .find_each do |project_storage|
-        permissions = admin_remote_identities_scope
-                        .pluck(:origin_user_id)
-                        .map do |origin_user_id|
-          { user_id: origin_user_id, permissions: [:write_files] }
-        end
-
-        project_remote_identities(project_storage).each do |identity|
-          add_user_to_permission_list(permissions, identity, project_storage.project)
-        end
-
-        info "Setting permissions for #{project_storage.managed_project_folder_name}: #{permissions}"
-
-        project_folder_id = project_storage.project_folder_id
-        build_permissions_input_data(project_folder_id, permissions)
-          .either(
-            ->(input_data) { set_permissions.call(storage: @storage, auth_strategy:, input_data:) },
-            ->(failure) { log_validation_error(failure, project_folder_id:, permissions:) }
-          )
-      end
-    end
-
-    # rubocop:enable Metrics/AbcSize
-
     def ensure_folders_exist(folder_map)
       info "Ensuring that automatically managed project folders exist and are correctly named."
-      active_project_storages_scope.includes(:project).find_each do |project_storage|
+      @project_storages.includes(:project).find_each do |project_storage|
         unless folder_map.key?(project_storage.project_folder_id)
           info "#{project_storage.managed_project_folder_path} does not exist. Creating..."
           next create_remote_folder(project_storage.managed_project_folder_path, project_storage.id)
@@ -131,17 +98,7 @@ module Storages
     end
 
     def inactive_folder_ids(folder_map)
-      folder_map.keys - active_project_storages_scope.pluck(:project_folder_id).compact
-    end
-
-    def add_user_to_permission_list(permissions, identity, project)
-      op_user_permissions = identity.user.all_permissions_for(project)
-
-      if op_user_permissions.member?(:write_files)
-        permissions << { user_id: identity.origin_user_id, permissions: [:write_files] }
-      elsif op_user_permissions.member?(:read_files)
-        permissions << { user_id: identity.origin_user_id, permissions: [:read_files] }
-      end
+      folder_map.keys - @project_storages.pluck(:project_folder_id).compact
     end
 
     def rename_project_folder(current_folder_name, project_storage)
@@ -208,48 +165,22 @@ module Storages
       folders
     end
 
-    def project_remote_identities(project_storage)
-      project_remote_identities = client_remote_identities_scope.where.not(id: admin_remote_identities_scope).order(:id)
-
-      if project_storage.project.public? && ProjectRole.non_member.permissions.intersect?(OP_PERMISSIONS)
-        project_remote_identities
-      else
-        project_remote_identities.where(user: project_storage.project.users)
-      end
-    end
-
-    def active_project_storages_scope
-      @storage.project_storages.active.automatic
-    end
-
-    def client_remote_identities_scope
-      RemoteIdentity.includes(:user).where(integration: @storage)
-    end
-
-    def admin_remote_identities_scope
-      client_remote_identities_scope.where(user: User.admin.active)
-    end
-
     def root_folder = Peripherals::ParentFolder.new("/")
+
+    def create_folder = Peripherals::Registry.resolve("one_drive.commands.create_folder")
+
+    def rename_file = Peripherals::Registry.resolve("one_drive.commands.rename_file")
+
+    def set_permissions = Peripherals::Registry.resolve("one_drive.commands.set_permissions")
+
+    def files = Peripherals::Registry.resolve("one_drive.queries.files")
+
+    def userless = Peripherals::Registry.resolve("one_drive.authentication.userless")
 
     def auth_strategy = userless.call
 
     def build_permissions_input_data(file_id, user_permissions)
       Peripherals::StorageInteraction::Inputs::SetPermissions.build(file_id:, user_permissions:)
-    end
-
-    # @param attribute [Symbol] attribute to which the error will be tied to
-    # @param storage_error [Storages::StorageError] an StorageError instance
-    # @param options [Hash{Symbol => Object}] optional extra parameters for the message generation
-    # @return ServiceResult
-    def add_error(attribute, storage_error, options: {})
-      case storage_error.code
-      when :error, :unauthorized
-        @result.errors.add(:base, storage_error.code, **options)
-      else
-        @result.errors.add(attribute, storage_error.code, **options)
-      end
-      @result
     end
   end
 end

modules/storages/app/services/storages/one_drive_managed_folder_permissions_service.rb
@@ -1,0 +1,128 @@
+# frozen_string_literal: true
+
+#-- copyright
+# OpenProject is an open source project management software.
+# Copyright (C) the OpenProject GmbH
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License version 3.
+#
+# OpenProject is a fork of ChiliProject, which is a fork of Redmine. The copyright follows:
+# Copyright (C) 2006-2013 Jean-Philippe Lang
+# Copyright (C) 2010-2013 the ChiliProject Team
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+# See COPYRIGHT and LICENSE files for more details.
+#++
+
+module Storages
+  class OneDriveManagedFolderPermissionsService < BaseService
+    using Peripherals::ServiceResultRefinements
+
+    OP_PERMISSIONS = %i[read_files write_files create_files delete_files share_files].freeze
+
+    def self.i18n_key = "one_drive_sync_service"
+
+    class << self
+      def call(storage:, project_storages_scope: nil)
+        new(storage:, project_storages_scope:).call
+      end
+    end
+
+    def initialize(storage:, project_storages_scope: nil)
+      super()
+      @storage = storage
+      @project_storages = (project_storages_scope || @storage.project_storages).active.automatic
+    end
+
+    def call
+      with_tagged_logger([self.class.name, "storage-#{@storage.id}"]) do
+        apply_permission_to_folders
+
+        @result
+      end
+    end
+
+    private
+
+    # rubocop:disable Metrics/AbcSize
+    def apply_permission_to_folders
+      info "Setting permissions to project folders"
+      @project_storages.includes(:project)
+                                   .where.not(project_folder_id: nil)
+                                   .find_each do |project_storage|
+        permissions = admin_remote_identities_scope
+                        .pluck(:origin_user_id)
+                        .map do |origin_user_id|
+          { user_id: origin_user_id, permissions: [:write_files] }
+        end
+
+        project_remote_identities(project_storage).each do |identity|
+          add_user_to_permission_list(permissions, identity, project_storage.project)
+        end
+
+        info "Setting permissions for #{project_storage.managed_project_folder_name}: #{permissions}"
+
+        project_folder_id = project_storage.project_folder_id
+        build_permissions_input_data(project_folder_id, permissions)
+          .either(
+            ->(input_data) { set_permissions.call(storage: @storage, auth_strategy:, input_data:) },
+            ->(failure) { log_validation_error(failure, project_folder_id:, permissions:) }
+          )
+      end
+    end
+
+    # rubocop:enable Metrics/AbcSize
+
+    def add_user_to_permission_list(permissions, identity, project)
+      op_user_permissions = identity.user.all_permissions_for(project)
+
+      if op_user_permissions.member?(:write_files)
+        permissions << { user_id: identity.origin_user_id, permissions: [:write_files] }
+      elsif op_user_permissions.member?(:read_files)
+        permissions << { user_id: identity.origin_user_id, permissions: [:read_files] }
+      end
+    end
+
+    def project_remote_identities(project_storage)
+      project_remote_identities = client_remote_identities_scope.where.not(id: admin_remote_identities_scope).order(:id)
+
+      if project_storage.project.public? && ProjectRole.non_member.permissions.intersect?(OP_PERMISSIONS)
+        project_remote_identities
+      else
+        project_remote_identities.where(user: project_storage.project.users)
+      end
+    end
+
+    def client_remote_identities_scope
+      RemoteIdentity.includes(:user).where(integration: @storage)
+    end
+
+    def admin_remote_identities_scope
+      client_remote_identities_scope.where(user: User.admin.active)
+    end
+
+    def set_permissions = Peripherals::Registry.resolve("one_drive.commands.set_permissions")
+
+    def userless = Peripherals::Registry.resolve("one_drive.authentication.userless")
+
+    def auth_strategy = userless.call
+
+    def build_permissions_input_data(file_id, user_permissions)
+      Peripherals::StorageInteraction::Inputs::SetPermissions.build(file_id:, user_permissions:)
+    end
+  end
+end