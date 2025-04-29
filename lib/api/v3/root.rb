b49977bbb7ce11ca35dd7a202afd62fbe6ad24e6
ulferts <jens.ulferts@googlemail.com>
2025-04-29 17:11:54 +0200

    add a project phase definition show endpoint

--- lib/api/v3/root.rb
+++ lib/api/v3/root.rb
@@ -66,6 +66,7 @@ module API
       mount ::API::V3::Principals::PrincipalsAPI
       mount ::API::V3::Priorities::PrioritiesAPI
       mount ::API::V3::Projects::ProjectsAPI
+      mount ::API::V3::ProjectPhaseDefinitions::ProjectPhaseDefinitionsAPI
       mount ::API::V3::ProjectPhases::ProjectPhasesAPI
       mount ::API::V3::Projects::Statuses::StatusesAPI
       mount ::API::V3::Queries::QueriesAPI
