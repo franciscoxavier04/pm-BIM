4c39f5ffdcfecc88706f7ebff702fa05f15a9fba
Behrokh Satarnejad <b.satarnejad@openproject.com>
2025-04-30 10:39:17 +0200

    remove role: alert

--- app/controllers/work_package_relations_controller.rb
+++ app/controllers/work_package_relations_controller.rb
@@ -111,8 +111,7 @@ class WorkPackageRelationsController < ApplicationController
       render_success_flash_message_via_turbo_stream(message: I18n.t(:notice_successful_update))
       render_aria_update_message(
         message: I18n.t("work_package_relations_tab.relations.create_relation_aria_live_message"),
-        type: "polite",
-        role: "alert"
+        type: "polite"
       )
       respond_with_turbo_streams
     else
