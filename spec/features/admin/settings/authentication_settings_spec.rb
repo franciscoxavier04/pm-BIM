c2dda00c518ce11205179126a1d64c01a3ada925
Oliver Günther <mail@oliverguenther.de>
2025-05-06 14:18:12 +0200

    Fix input group selection

---
+++ spec/features/admin/settings/authentication_settings_spec.rb
@@ -0,0 +1,66 @@
+# frozen_string_literal: true
+
+#-- copyright
+# OpenProject is an open source project management software.
+# Copyright (C) the OpenProject GmbH
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License version 3.
+#
+# OpenProject is a fork of ChiliProject, which is a fork of Redmine. The copyright follows:
+# Copyright (C) 2006-2013 Jean-Philippe Lang
+# Copyright (C) 2010-2013 the ChiliProject Team
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+# See COPYRIGHT and LICENSE files for more details.
+#++
+
+require "spec_helper"
+
+RSpec.describe "Authentication Settings" do
+  shared_let(:admin) { create(:admin) }
+
+  before do
+    login_as(admin)
+  end
+
+  describe "self registration settings" do
+    it "allows changing self registration options" do
+      visit admin_settings_authentication_path(tab: "registration")
+
+      # Test changing to "0" (disabled)
+      choose I18n.t(:setting_self_registration_disabled)
+      click_button "Save"
+      expect(page).to have_field("settings[self_registration]",
+                                 with: Setting::SelfRegistration.value(key: :disabled))
+
+      choose I18n.t(:setting_self_registration_activation_by_email)
+      click_button "Save"
+      expect(page).to have_field("settings[self_registration]",
+                                 with: Setting::SelfRegistration.value(key: :activation_by_email))
+
+      choose I18n.t(:setting_self_registration_manual_activation)
+      click_button "Save"
+      expect(page).to have_field("settings[self_registration]",
+                                 with: Setting::SelfRegistration.value(key: :manual_activation))
+
+      choose I18n.t(:setting_self_registration_automatic_activation)
+      click_button "Save"
+      expect(page).to have_field("settings[self_registration]",
+                                 with: Setting::SelfRegistration.value(key: :automatic_activation))
+    end
+  end
+end
