e6f988d5c00071328789e4e9fa46b530b2d422b0
Kabiru Mwenja <k.mwenja@openproject.com>
2025-05-07 14:26:45 +0300

    Use the correct stimulus outlet selector for internal comments

--- app/components/work_packages/activities_tab/index_component.rb
+++ app/components/work_packages/activities_tab/index_component.rb
@@ -82,7 +82,7 @@ module WorkPackages
           action: index_stimulus_controller(":onSubmit-end@window->#{internal_comment_stimulus_controller}#onSubmitEnd"),
           internal_comment_stimulus_controller("-highlight-class") => "work-packages-activities-tab-journals-new-component--journal-notes-body__internal-comment", # rubocop:disable Layout/LineLength
           internal_comment_stimulus_controller("-hidden-class") => "d-none",
-          internal_comment_stimulus_controller("-#{index_stimulus_controller}-outlet") => "##{wrapper_key}",
+          internal_comment_stimulus_controller("-#{index_stimulus_controller}-outlet") => index_stimulus_controller_outlet_selector, # rubocop:disable Layout/LineLength
           internal_comment_stimulus_controller("-is-internal-value") => false # Initial value
         }
       end
