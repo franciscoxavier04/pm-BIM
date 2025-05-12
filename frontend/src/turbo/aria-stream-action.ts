434931b862cd1f82a795bd9065ac7e2b7148240f
Behrokh Satarnejad <b.satarnejad@openproject.com>
2025-05-12 16:36:32 +0200

    set delay for polite updates

--- frontend/src/turbo/aria-stream-action.ts
+++ frontend/src/turbo/aria-stream-action.ts
@@ -3,17 +3,19 @@ import { announce } from '@primer/live-region-element';
 
 export function registerAriaStreamAction() {
   StreamActions.aria = function ariaStreamAction(this:StreamElement) {
-      const message = this.getAttribute('message') || '';
-      const politeness = this.getAttribute('politeness') || 'polite';
-      if (politeness === 'assertive') {
-        void announce(message, {
-          politeness: 'assertive',
-        });
-      } else {
-        void announce(message, {
-          politeness: 'polite',
-          delayMs: 5000,
-        });
-      }
+    const message = this.getAttribute('message') || '';
+    const politeness = this.getAttribute('politeness') || 'polite';
+    const delayAttr = this.getAttribute('delay');
+    const delay = delayAttr !== null && !Number.isNaN(Number(delayAttr)) ? Number(delayAttr) : 0;
+    if (politeness === 'assertive') {
+      void announce(message, {
+        politeness: 'assertive',
+      });
+    } else {
+      void announce(message, {
+        politeness: 'polite',
+        delayMs: delay,
+      });
+    }
   };
 }
