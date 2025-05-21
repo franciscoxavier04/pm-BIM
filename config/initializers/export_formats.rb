nimblePM/nimblePM

activate pdf export for project_phases

config/initializers/export_formats.rb
@@ -6,17 +6,18 @@ Rails.application.configure do |application|
 
       single WorkPackage, WorkPackage::PDFExport::WorkPackageToPdf
 
-      formatter WorkPackage, WorkPackage::Exports::Formatters::EstimatedHours
-      formatter WorkPackage, WorkPackage::Exports::Formatters::RemainingHours
-      formatter WorkPackage, WorkPackage::Exports::Formatters::SpentUnits
-      formatter WorkPackage, WorkPackage::Exports::Formatters::Hours
-      formatter WorkPackage, WorkPackage::Exports::Formatters::Days
-      formatter WorkPackage, WorkPackage::Exports::Formatters::Currency
-      formatter WorkPackage, WorkPackage::Exports::Formatters::Costs
-      formatter WorkPackage, WorkPackage::Exports::Formatters::DoneRatio
-      formatter WorkPackage, WorkPackage::Exports::Formatters::Date
       formatter WorkPackage, Exports::Formatters::CustomField
       formatter WorkPackage, Exports::Formatters::CustomFieldPdf
+      formatter WorkPackage, WorkPackage::Exports::Formatters::Costs
+      formatter WorkPackage, WorkPackage::Exports::Formatters::Currency
+      formatter WorkPackage, WorkPackage::Exports::Formatters::Date
+      formatter WorkPackage, WorkPackage::Exports::Formatters::Days
+      formatter WorkPackage, WorkPackage::Exports::Formatters::DoneRatio
+      formatter WorkPackage, WorkPackage::Exports::Formatters::EstimatedHours
+      formatter WorkPackage, WorkPackage::Exports::Formatters::Hours
+      formatter WorkPackage, WorkPackage::Exports::Formatters::ProjectPhase
+      formatter WorkPackage, WorkPackage::Exports::Formatters::RemainingHours
+      formatter WorkPackage, WorkPackage::Exports::Formatters::SpentUnits
 
       list Project, Projects::Exports::CSV
       formatter Project, Exports::Formatters::CustomField

franciscoxavier04/pm-BIM

config/initializers/export_formats.rb
@@ -36,17 +36,18 @@ Rails.application.configure do |application|
 
       single WorkPackage, WorkPackage::PDFExport::WorkPackageToPdf
 
-      formatter WorkPackage, WorkPackage::Exports::Formatters::CompoundHours
-      formatter WorkPackage, WorkPackage::Exports::Formatters::SpentUnits
-      formatter WorkPackage, WorkPackage::Exports::Formatters::Hours
-      formatter WorkPackage, WorkPackage::Exports::Formatters::Days
-      formatter WorkPackage, WorkPackage::Exports::Formatters::Currency
-      formatter WorkPackage, WorkPackage::Exports::Formatters::Costs
-      formatter WorkPackage, WorkPackage::Exports::Formatters::CompoundDoneRatio
-      formatter WorkPackage, WorkPackage::Exports::Formatters::DoneRatio
-      formatter WorkPackage, WorkPackage::Exports::Formatters::Date
       formatter WorkPackage, Exports::Formatters::CustomField
       formatter WorkPackage, Exports::Formatters::CustomFieldPdf
+      formatter WorkPackage, WorkPackage::Exports::Formatters::CompoundDoneRatio
+      formatter WorkPackage, WorkPackage::Exports::Formatters::CompoundHours
+      formatter WorkPackage, WorkPackage::Exports::Formatters::Costs
+      formatter WorkPackage, WorkPackage::Exports::Formatters::Currency
+      formatter WorkPackage, WorkPackage::Exports::Formatters::Date
+      formatter WorkPackage, WorkPackage::Exports::Formatters::Days
+      formatter WorkPackage, WorkPackage::Exports::Formatters::DoneRatio
+      formatter WorkPackage, WorkPackage::Exports::Formatters::Hours
+      formatter WorkPackage, WorkPackage::Exports::Formatters::ProjectPhase
+      formatter WorkPackage, WorkPackage::Exports::Formatters::SpentUnits
 
       list Project, Projects::Exports::CSV
       formatter Project, Exports::Formatters::CustomField