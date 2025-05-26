2dcb128dfd0de223a6f090b7403e1e55c8d83f07
Behrokh Satarnejad <b.satarnejad@openproject.com>
2025-05-26 15:22:06 +0200

    fix failing test

--- spec/lib/redmine/menu_manager/menu_helper_spec.rb
+++ spec/lib/redmine/menu_manager/menu_helper_spec.rb
@@ -132,7 +132,7 @@ RSpec.describe Redmine::MenuManager::MenuHelper, type: :helper do
               </button>
             </div>
             <div class="main-menu--children-menu-header">
-              <a title="Up" class="main-menu--arrow-left-to-project" data-action="menus--main#ascend" data-tour-selector="main-menu--arrow-left_parent_node">
+              <a title="Up" class="main-menu--arrow-left-to-project" data-action="menus--main#ascend" data-tour-selector="main-menu--arrow-left_parent_node" href="#" aria-label="Go back">
                 <svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-arrow-left">
                   <path d="M7.78 12.53a.75.75 0 0 1-1.06 0L2.47 8.28a.75.75 0 0 1 0-1.06l4.25-4.25a.751.751 0 0 1 1.042.018.751.751 0 0 1 .018 1.042L4.81 7h7.44a.75.75 0 0 1 0 1.5H4.81l2.97 2.97a.75.75 0 0 1 0 1.06Z"></path>
                 </svg>
