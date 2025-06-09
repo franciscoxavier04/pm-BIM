85fd7478d42ed004b94a9add1d3916c1d8635a91
Alexander Brandon Coles <a.coles@openproject.com>
2025-06-09 18:17:48 +0100

    Make AttributeHelpTextComponent render QA data
    Renders `data-qa-help-text-for` for interop with existing feature specs.

--- spec/components/open_project/common/attribute_help_text_component_spec.rb
+++ spec/components/open_project/common/attribute_help_text_component_spec.rb
@@ -67,6 +67,16 @@ RSpec.describe OpenProject::Common::AttributeHelpTextComponent, type: :component
     it "applies an ID" do
       expect(subject).to have_element :a, id: /attribute-help-text-component-\d+/
     end
+
+    it "defines a data-qa-help-text-for attribute" do
+      expect(subject).to have_element :a, "data-qa-help-text-for": attribute_name.to_s.camelize(:lower)
+    end
+
+    context "without an additional label" do
+      it "does not render additional label content" do
+        expect(subject).to have_no_css ".Link-content"
+      end
+    end
   end
 
   shared_examples "component does not render" do
@@ -80,15 +90,35 @@ RSpec.describe OpenProject::Common::AttributeHelpTextComponent, type: :component
   end
 
   context "with a project help text" do
-    let(:help_text) { build_stubbed(:project_help_text) }
+    let(:help_text) { build_stubbed(:project_help_text, attribute_name:) }
 
-    it_behaves_like "component renders"
+    context "for a default attribute" do
+      let(:attribute_name) { :name }
+
+      it_behaves_like "component renders"
+    end
+
+    context "for a project attribute" do
+      let(:attribute_name) { :custom_field_2105 }
+
+      it_behaves_like "component renders"
+    end
   end
 
   context "with a work package help text" do
-    let(:help_text) { build_stubbed(:work_package_help_text) }
+    let(:help_text) { build_stubbed(:work_package_help_text, attribute_name:) }
 
-    it_behaves_like "component renders"
+    context "for a default attribute" do
+      let(:attribute_name) { :subject }
+
+      it_behaves_like "component renders"
+    end
+
+    context "for a custom field attribute" do
+      let(:attribute_name) { :custom_field_99 }
+
+      it_behaves_like "component renders"
+    end
   end
 
   context "with nil help text" do
