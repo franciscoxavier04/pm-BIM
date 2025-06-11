33e8b319c55947d32e698aaa96b1ba51231b690b
Eric Schubert <e.schubert@openproject.com>
2025-06-11 16:48:03 +0200

    [#64607] added work package type create service
    - https://community.openproject.org/work_packages/64607
    - add new create service deriving from base_contracted
    - add shallow ghost of set_attribute and create_contract

--- app/controllers/types_controller.rb
+++ app/controllers/types_controller.rb
@@ -59,18 +59,16 @@ class TypesController < ApplicationController
   end
 
   def create
-    CreateTypeService
-      .new(current_user)
-      .call(permitted_type_params, copy_workflow_from: params.dig(:type, :copy_workflow_from)) do |call|
-      @type = call.result
+    additional_params = { copy_workflow_from: params.dig(:type, :copy_workflow_from) }
+    service_call = WorkPackageTypes::CreateService
+                     .new(user: current_user)
+                     .call(permitted_type_params.merge(additional_params))
 
-      call.on_success do
-        redirect_to_type_tab_path(@type, t(:notice_successful_create))
-      end
-
-      call.on_failure do
-        render action: :new, status: :unprocessable_entity
-      end
+    @type = service_call.result
+    if service_call.success?
+      redirect_to_type_tab_path(@type, t(:notice_successful_create))
+    else
+      render action: :new, status: :unprocessable_entity
     end
   end
 
