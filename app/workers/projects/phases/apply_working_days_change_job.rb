46ef0c505f7bb2a42766f91508c2c68b7ecea74d
Dombi Attila <83396+dombesz@users.noreply.github.com>
2025-06-16 14:52:24 +0300

    [#64836] Incorrect rescheduling after non working days are changed
    https://community.openproject.org/work_packages/64836
    
    Change Project::Phase::ApplyWorkingDaysChangeJob to reschedule only from the first phase that has a start date.

--- app/workers/projects/phases/apply_working_days_change_job.rb
+++ app/workers/projects/phases/apply_working_days_change_job.rb
@@ -33,15 +33,14 @@ class Projects::Phases::ApplyWorkingDaysChangeJob < ApplyWorkingDaysChangeJobBas
 
   def apply_working_days_change
     Project.where(id: applicable_phases.select(:project_id)).find_each do |project|
-      phases = project.available_phases.to_a
-      from = phases.filter_map(&:start_date).first
+      available_phases = project.available_phases.to_a
+      phase = available_phases.find(&:start_date?)
+      from = phase&.start_date
       next unless from
 
-      ProjectPhases::RescheduleService.new(user: User.current, project:).call(phases:, from:)
+      phases = available_phases.filter { it.position >= phase.position }
 
-      project.journal_cause = journal_cause
-
-      project.touch_and_save_journals
+      reschedule_project_phases(project:, phases:, from:)
     end
   end
 
@@ -52,5 +51,13 @@ class Projects::Phases::ApplyWorkingDaysChangeJob < ApplyWorkingDaysChangeJobBas
     Project::Phase
       .active
       .covering_dates_or_days_of_week(days_of_week:, dates:)
+  end
+
+  def reschedule_project_phases(project:, phases:, from:)
+    ProjectPhases::RescheduleService.new(user: User.current, project:).call(phases:, from:)
+
+    project.journal_cause = journal_cause
+
+    project.touch_and_save_journals
   end
 end
