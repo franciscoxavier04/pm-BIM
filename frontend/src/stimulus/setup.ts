73ea764df13cea0daa9b589265ba6ff6b989b9bb
Oliver Günther <mail@oliverguenther.de>
2025-06-17 10:29:36 +0200

    Remove application dynamic target

--- frontend/src/stimulus/setup.ts
+++ frontend/src/stimulus/setup.ts
@@ -39,7 +39,6 @@ instance.handleError = (error, message, detail) => {
   console.warn(error, message, detail);
 };
 
-instance.register('application', OpApplicationController);
 instance.register('async-dialog', AsyncDialogController);
 instance.register('disable-when-checked', OpDisableWhenCheckedController);
 instance.register('flash', FlashController);
@@ -61,5 +60,8 @@ instance.register('scroll-into-view', ScrollIntoViewController);
 instance.register('ckeditor-focus', CkeditorFocusController);
 instance.register('add-meeting-params', AddMeetingParamsController);
 instance.register('highlight-when-value-selected', HighlightWhenValueSelectedController);
-
 instance.register('auto-submit', AutoSubmit);
+
+// Application controller must be registered last, as it tries to automatically load other controllers
+// not yet registered.
+instance.register('application', OpApplicationController);
