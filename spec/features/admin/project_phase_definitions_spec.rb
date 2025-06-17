543d54953624c921b59d699a0a35c2544e50bcb1
Ivan Kuchin <i.kuchin@openproject.com>
2025-06-17 12:51:35 +0200

    rename step definitions spec

--- spec/features/admin/project_phase_definitions_spec.rb
+++ spec/features/admin/project_phase_definitions_spec.rb
@@ -30,7 +30,7 @@
 
 require "spec_helper"
 
-RSpec.describe "Projects life cycle settings", :js, with_flag: { stages_and_gates: true } do
+RSpec.describe "Projects phase definition settings", :js, with_flag: { stages_and_gates: true } do
   shared_let(:initiating_stage) { create(:project_phase_definition, name: "Initiating") }
   shared_let(:executing_stage) do
     create(:project_phase_definition, name: "Executing", start_gate: true, start_gate_name: "Ready to Execute")
