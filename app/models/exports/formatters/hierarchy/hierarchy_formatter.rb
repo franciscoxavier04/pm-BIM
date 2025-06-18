56aef103772049eb4e2b88105adfdff4412dcf82
as-op <a.sandorf@openproject.com>
2025-06-18 12:53:01 +0200

    [#60404] Hierarchy custom field - pdf output display only last item without hierarchy
    https://community.openproject.org/work_packages/60404

--- app/models/exports/formatters/hierarchy/hierarchy_formatter.rb
+++ app/models/exports/formatters/hierarchy/hierarchy_formatter.rb
@@ -30,22 +30,34 @@
 
 module Exports
   module Formatters
-    class CustomFieldPdf < CustomField
-      def self.apply?(attribute, export_format)
-        export_format == :pdf && attribute.start_with?("cf_")
-      end
+    module Hierarchy
+      class HierarchyFormatter
+        def format(object, custom_field)
+          cvs = object.custom_value_for(custom_field)
+          case cvs
+          when Array
+            cvs.map { |item| format_hierarchy_item_for_export(item) }.join(", ")
+          when CustomValue
+            format_hierarchy_item_for_export(cvs)
+          else
+            cvs
+          end
+        end
 
-      ##
-      # Print the value meant for PDF export.
-      #
-      # - For boolean values, use the Yes/No formatting for the PDF
-      #   treat nil as false
-      def format_for_export(object, custom_field)
-        if custom_field.field_format == "bool"
-          value = object.typed_custom_value_for(custom_field)
-          value ? I18n.t(:general_text_Yes) : I18n.t(:general_text_No)
-        else
-          super
+        def format_hierarchy_item_for_export(item_value)
+          item = ::CustomField::Hierarchy::Item.find_by(id: item_value.to_s)
+          return "#{item_value} #{I18n.t(:label_not_found)}" if item.nil?
+
+          nodes = hierarchy_item_service.get_branch(item:).value!
+          nodes
+            .reject(&:root?)
+            .map do |branch_item|
+            branch_item.short.present? ? "#{branch_item.label} (#{branch_item.short})" : branch_item.label
+          end.join(" / ")
+        end
+
+        def hierarchy_item_service
+          ::CustomFields::Hierarchy::HierarchicalItemService.new
         end
       end
     end
