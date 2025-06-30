bf1af534745feabe6ae4ba56bf0811c65f278f0f
Tobias Dillmann <github@pentokami.de>
2025-06-30 10:47:05 +0200

    [#64347] is_for_all -> is_required

--- app/components/admin/custom_fields/calculated_values/details_form.rb
+++ app/components/admin/custom_fields/calculated_values/details_form.rb
@@ -61,9 +61,9 @@ module Admin::CustomFields::CalculatedValues
       )
 
       details_form.check_box(
-        name: :is_for_all,
-        label: I18n.t("activerecord.attributes.custom_field.is_for_all"),
-        caption: I18n.t("custom_fields.instructions.is_for_all")
+        name: :is_required,
+        label: I18n.t("activerecord.attributes.project_custom_field.is_required"),
+        caption: I18n.t("custom_fields.instructions.is_required_for_project")
       )
 
       details_form.check_box(
