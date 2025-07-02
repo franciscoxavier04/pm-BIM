nimblePM/nimblePM

Ensure we treat the scheduled start_time in the series time zone

modules/meeting/app/models/meeting/virtual_start_time.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 #-- copyright
 # OpenProject is an open source project management software.
 # Copyright (C) the OpenProject GmbH
@@ -112,8 +113,8 @@ module Meeting::VirtualStartTime
   end
 
   def update_derived_fields
-    @start_date = format_time_as_date(start_time, format: "%Y-%m-%d")
-    @start_time_hour = format_time(start_time, include_date: false, format: "%H:%M")
+    @start_date = format_time_as_date(start_time, time_zone:, format: "%Y-%m-%d")
+    @start_time_hour = format_time(start_time, time_zone:, include_date: false, format: "%H:%M")
   end
 
   ##

franciscoxavier04/pm-BIM

modules/meeting/app/models/meeting/virtual_start_time.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 #-- copyright
 # OpenProject is an open source project management software.
 # Copyright (C) the OpenProject GmbH
@@ -112,8 +113,8 @@ module Meeting::VirtualStartTime
   end
 
   def update_derived_fields
-    @start_date = format_date(start_time, format: "%Y-%m-%d")
-    @start_time_hour = format_time(start_time, include_date: false, format: "%H:%M")
+    @start_date = format_date(start_time, time_zone:, format: "%Y-%m-%d")
+    @start_time_hour = format_time(start_time, time_zone:, include_date: false, format: "%H:%M")
   end
 
   ##