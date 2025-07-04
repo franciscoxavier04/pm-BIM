fba88c5c0198bd1b7e863ffa8740776a106079bf
Eric Schubert <e.schubert@openproject.com>
2025-07-04 13:14:39 +0200

    [#61937] add absolute position to pattern input dropdown
    - https://community.openproject.org/work_packages/61937

--- app/components/work_package_types/pattern_input.rb
+++ app/components/work_package_types/pattern_input.rb
@@ -49,8 +49,11 @@ module WorkPackageTypes
       @suggestions_list_component ||= Primer::Alpha::ActionList.new(
         role: :list,
         scheme: :inset,
+        classes: "op-pattern-input--suggestions-dropdown",
         ml: 0,
-        "data-pattern-input-target": "suggestions"
+        data: {
+          "pattern-input-target": "suggestions"
+        }
       )
     end
   end
