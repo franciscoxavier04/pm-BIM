f11dcd10dbc706175a45bf8e80fd2894fec03f4c
Ivan Kuchin <i.kuchin@openproject.com>
2025-07-07 16:21:56 +0200

    join order by name spec into version spec file

--- spec/models/version_spec.rb
+++ spec/models/version_spec.rb
@@ -571,4 +571,31 @@ RSpec.describe Version do
       end
     end
   end
+
+  describe "order by name" do
+    let(:project) { create(:project) }
+    let(:names) do
+      [
+        "1. xxxx",
+        "1.1. aaa",
+        "1.1. zzz",
+        "1.2. mmm",
+        "1.10. aaa",
+        "9",
+        "10.2",
+        "10.10.2",
+        "10.10.10",
+        "aaaaa",
+        "aaaaa 1."
+      ]
+    end
+    let!(:versions) { names.map { |name| create(:version, name:, project:) } }
+
+    subject { described_class.order(:name, id: :desc).to_a }
+
+    it "returns the versions in semver order" do
+      expect(subject)
+        .to eql versions
+    end
+  end
 end
