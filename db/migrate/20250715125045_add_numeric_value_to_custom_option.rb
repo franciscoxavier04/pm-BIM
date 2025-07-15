d1266f2131305525e8cddd6e0cbf97837309ca14
Ivan Kuchin <i.kuchin@openproject.com>
2025-07-15 16:29:38 +0200

    note on column type

--- db/migrate/20250715125045_add_numeric_value_to_custom_option.rb
+++ db/migrate/20250715125045_add_numeric_value_to_custom_option.rb
@@ -2,6 +2,7 @@
 
 class AddNumericValueToCustomOption < ActiveRecord::Migration[8.0]
   def change
+    # it is probably better to use string mapped to big decimal, as it should handle arbitary precision
     add_column :custom_options, :numeric_value, :float, null: true
   end
 end
