783f1a2ec94b8a197512c2d211cd17e258382837
Oliver Günther <mail@oliverguenther.de>
2025-07-15 14:16:48 +0200

    Create risk dialog

--- app/forms/work_packages/dialogs/create_form.rb
+++ app/forms/work_packages/dialogs/create_form.rb
@@ -113,7 +113,20 @@ module WorkPackages::Dialogs
     private
 
     def custom_fields
-      @custom_fields ||= work_package.available_custom_fields.select(&:required?)
+      @custom_fields ||= begin
+        required = work_package.available_custom_fields.select(&:required?)
+        if work_package.type == BmdsHackathon::References.risk_type
+          risk_cfs= [
+            BmdsHackathon::References.risk_likelihood_cf,
+            BmdsHackathon::References.risk_impact_cf,
+            BmdsHackathon::References.risk_level_cf
+          ]
+
+          risk_cfs + required
+        else
+          required
+        end
+      end
     end
 
     def writable_attributes
