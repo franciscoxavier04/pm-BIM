Modularsoft/openproject

Allow to synchronize groups via OpenID Connect
When the corresponding claim is present and synchronization of groups
is enabled, then we'll import groups from the groups claim.

modules/openid_connect/app/services/openid_connect/groups/sync_service.rb
@@ -1,0 +1,111 @@
+# frozen_string_literal: true
+
+#-- copyright
+# OpenProject is an open source project management software.
+# Copyright (C) the OpenProject GmbH
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License version 3.
+#
+# OpenProject is a fork of ChiliProject, which is a fork of Redmine. The copyright follows:
+# Copyright (C) 2006-2013 Jean-Philippe Lang
+# Copyright (C) 2010-2013 the ChiliProject Team
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+# See COPYRIGHT and LICENSE files for more details.
+#++
+
+module OpenIDConnect
+  module Groups
+    class SyncService
+      def initialize(user:)
+        @user = user
+        @result = ServiceResult.success
+      end
+
+      def call(groups_claim:)
+        raise ArgumentError, "groups_claim must not be nil" if groups_claim.nil?
+
+        user_groups = []
+
+        groups_claim.each do |oidc_name|
+          group = process_oidc_group(oidc_name)
+          user_groups << group if group
+        end
+
+        remove_groups_except(user_groups)
+
+        @result
+      end
+
+      private
+
+      def process_oidc_group(oidc_name)
+        local_name = filter_and_name_group(oidc_name)
+        return nil if local_name.nil?
+
+        group = find_group(oidc_name, local_name)
+        update_group_users(group) { |user_ids| (user_ids + [@user.id]).uniq }
+
+        membership = group.group_users.find_by(user: @user)
+        membership.oidc_group_memberships.find_or_create_by!(auth_provider: authentication_provider)
+
+        group
+      end
+
+      def remove_groups_except(keep_groups)
+        (@user.groups - keep_groups).each do |group|
+          update_group_users(group) { |user_ids| user_ids - [@user.id] }
+        end
+      end
+
+      def filter_and_name_group(group_name)
+        matcher = authentication_provider.group_matchers.find { |m| m.match?(group_name) }
+        return nil if matcher.nil?
+
+        match = matcher.match(group_name)
+        if match.size > 1
+          match[1..].join.presence
+        else
+          group_name
+        end
+      end
+
+      def find_group(oidc_name, local_name)
+        group_link = GroupLink.find_or_initialize_by(auth_provider: authentication_provider, oidc_name:)
+        if group_link.group.nil?
+          group_link.group = ::Group.find_or_create_by!(name: local_name)
+          group_link.save!
+        end
+
+        group_link.group
+      end
+
+      def update_group_users(group)
+        # TODO: is that service well suited for many small group changes?
+        current_user_ids = group.group_users.pluck(:user_id)
+        new_user_ids = yield current_user_ids
+        @result.merge!(
+          ::Groups::UpdateService.new(user: User.system, model: group).call(user_ids: new_user_ids)
+        )
+      end
+
+      def authentication_provider
+        @user.authentication_provider # TODO: should we find that differently? (risk of multiple auth providers)
+      end
+    end
+  end
+end

modules/openid_connect/db/migrate/20250710133700_add_oidc_group_links.rb
@@ -1,0 +1,43 @@
+# frozen_string_literal: true
+
+#-- copyright
+# OpenProject is an open source project management software.
+# Copyright (C) the OpenProject GmbH
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License version 3.
+#
+# OpenProject is a fork of ChiliProject, which is a fork of Redmine. The copyright follows:
+# Copyright (C) 2006-2013 Jean-Philippe Lang
+# Copyright (C) 2010-2013 the ChiliProject Team
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+# See COPYRIGHT and LICENSE files for more details.
+#++
+
+class AddOidcGroupLinks < ActiveRecord::Migration[8.0]
+  def change
+    create_table :oidc_group_links do |t|
+      t.belongs_to :group, null: false, index: true, foreign_key: { to_table: :users, on_delete: :cascade }
+
+      t.belongs_to :auth_provider, null: false, index: false, foreign_key: { on_delete: :cascade }
+      t.string :oidc_name, null: false
+      t.index %i[auth_provider_id oidc_name], unique: true
+
+      t.timestamps null: false
+    end
+  end
+end

modules/openid_connect/spec/factories/oidc_group_link_factory.rb
@@ -1,0 +1,37 @@
+# frozen_string_literal: true
+
+#-- copyright
+# OpenProject is an open source project management software.
+# Copyright (C) the OpenProject GmbH
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License version 3.
+#
+# OpenProject is a fork of ChiliProject, which is a fork of Redmine. The copyright follows:
+# Copyright (C) 2006-2013 Jean-Philippe Lang
+# Copyright (C) 2010-2013 the ChiliProject Team
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+# See COPYRIGHT and LICENSE files for more details.
+#++
+
+FactoryBot.define do
+  factory :oidc_group_link, class: "OpenIDConnect::GroupLink" do
+    group
+    auth_provider factory: :oidc_provider
+    sequence(:oidc_name) { |i| "oidc_group_#{i}" }
+  end
+end

franciscoxavier04/pm-BIM

modules/openid_connect/app/services/openid_connect/groups/sync_service.rb
@@ -1,0 +1,111 @@
+# frozen_string_literal: true
+
+#-- copyright
+# OpenProject is an open source project management software.
+# Copyright (C) the OpenProject GmbH
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License version 3.
+#
+# OpenProject is a fork of ChiliProject, which is a fork of Redmine. The copyright follows:
+# Copyright (C) 2006-2013 Jean-Philippe Lang
+# Copyright (C) 2010-2013 the ChiliProject Team
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+# See COPYRIGHT and LICENSE files for more details.
+#++
+
+module OpenIDConnect
+  module Groups
+    class SyncService
+      def initialize(user:)
+        @user = user
+        @result = ServiceResult.success
+      end
+
+      def call(groups_claim:)
+        raise ArgumentError, "groups_claim must not be nil" if groups_claim.nil?
+
+        user_groups = []
+
+        groups_claim.each do |oidc_group_name|
+          group = process_oidc_group(oidc_group_name)
+          user_groups << group if group
+        end
+
+        remove_groups_except(user_groups)
+
+        @result
+      end
+
+      private
+
+      def process_oidc_group(oidc_group_name)
+        oidc_group_name = filter_and_name_group(oidc_group_name)
+        return nil if oidc_group_name.nil?
+
+        group = find_group(oidc_group_name)
+        update_group_users(group) { |user_ids| (user_ids + [@user.id]).uniq }
+
+        membership = group.group_users.find_by(user: @user)
+        membership.oidc_group_memberships.find_or_create_by!(auth_provider: authentication_provider)
+
+        group
+      end
+
+      def remove_groups_except(keep_groups)
+        (@user.groups - keep_groups).each do |group|
+          update_group_users(group) { |user_ids| user_ids - [@user.id] }
+        end
+      end
+
+      def filter_and_name_group(group_name)
+        matcher = authentication_provider.group_matchers.find { |m| m.match?(group_name) }
+        return nil if matcher.nil?
+
+        match = matcher.match(group_name)
+        if match.size > 1
+          match[1..].join.presence
+        else
+          group_name
+        end
+      end
+
+      def find_group(oidc_group_name)
+        group_link = GroupLink.find_or_initialize_by(auth_provider: authentication_provider, oidc_group_name:)
+        if group_link.group.nil?
+          group_link.group = ::Group.find_or_create_by!(name: oidc_group_name)
+          group_link.save!
+        end
+
+        group_link.group
+      end
+
+      def update_group_users(group)
+        # TODO: is that service well suited for many small group changes?
+        current_user_ids = group.group_users.pluck(:user_id)
+        new_user_ids = yield current_user_ids
+        @result.merge!(
+          ::Groups::UpdateService.new(user: User.system, model: group).call(user_ids: new_user_ids)
+        )
+      end
+
+      def authentication_provider
+        @user.authentication_provider # TODO: should we find that differently? (risk of multiple auth providers)
+      end
+    end
+  end
+end

modules/openid_connect/db/migrate/20250710133700_add_oidc_group_links.rb
@@ -1,0 +1,43 @@
+# frozen_string_literal: true
+
+#-- copyright
+# OpenProject is an open source project management software.
+# Copyright (C) the OpenProject GmbH
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License version 3.
+#
+# OpenProject is a fork of ChiliProject, which is a fork of Redmine. The copyright follows:
+# Copyright (C) 2006-2013 Jean-Philippe Lang
+# Copyright (C) 2010-2013 the ChiliProject Team
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+# See COPYRIGHT and LICENSE files for more details.
+#++
+
+class AddOidcGroupLinks < ActiveRecord::Migration[8.0]
+  def change
+    create_table :oidc_group_links do |t|
+      t.belongs_to :group, null: false, index: true, foreign_key: { to_table: :users, on_delete: :cascade }
+
+      t.belongs_to :auth_provider, null: false, index: false, foreign_key: { on_delete: :cascade }
+      t.string :oidc_group_name, null: false
+      t.index %i[auth_provider_id oidc_group_name], unique: true
+
+      t.timestamps null: false
+    end
+  end
+end

modules/openid_connect/spec/factories/oidc_group_link_factory.rb
@@ -1,0 +1,37 @@
+# frozen_string_literal: true
+
+#-- copyright
+# OpenProject is an open source project management software.
+# Copyright (C) the OpenProject GmbH
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License version 3.
+#
+# OpenProject is a fork of ChiliProject, which is a fork of Redmine. The copyright follows:
+# Copyright (C) 2006-2013 Jean-Philippe Lang
+# Copyright (C) 2010-2013 the ChiliProject Team
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+# See COPYRIGHT and LICENSE files for more details.
+#++
+
+FactoryBot.define do
+  factory :oidc_group_link, class: "OpenIDConnect::GroupLink" do
+    group
+    auth_provider factory: :oidc_provider
+    sequence(:oidc_group_name) { |i| "oidc_group_#{i}" }
+  end
+end