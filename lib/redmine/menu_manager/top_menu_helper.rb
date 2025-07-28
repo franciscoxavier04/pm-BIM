f0939223493d6058a9247841aa3e5c334a70e99c
Behrokh Satarnejad <b.satarnejad@openproject.com>
2025-07-28 15:34:09 +0200

    change class name

--- lib/redmine/menu_manager/top_menu_helper.rb
+++ lib/redmine/menu_manager/top_menu_helper.rb
@@ -58,12 +58,12 @@ module Redmine::MenuManager::TopMenuHelper
   end
 
   def render_logo_icon
-    mode_class = User.current.pref.theme === "op-logo--link_high_contrast" ? "op-logo-icon" : "op-logo-icon_dark"
+    mode_class = User.current.pref.theme === "op-logo--link_high_contrast" ? "op-logo-icon" : "op-logo-icon_white"
     render Primer::BaseComponent.new(tag: :div, classes: mode_class)
   end
 
   def render_waffle_menu_logo_icon
-    mode_class = User.current.pref.theme === "dark" ? "op-logo-icon_dark" : "op-logo-icon"
+    mode_class = User.current.pref.theme === "dark" ? "op-logo-icon_white" : "op-logo-icon"
     render Primer::BaseComponent.new(tag: :div, classes: mode_class)
   end
 
