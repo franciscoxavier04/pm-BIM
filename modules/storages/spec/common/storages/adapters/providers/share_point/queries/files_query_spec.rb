goodtab/cpopenproject

Adds Root folder query capabilites

modules/storages/spec/common/storages/adapters/providers/share_point/queries/files_query_spec.rb
@@ -38,78 +38,79 @@ module Storages
         module Queries
           RSpec.describe FilesQuery, :webmock do
             let(:user) { create(:admin) }
-            let(:storage) { create(:share_point_storage, oauth_client_token_user: user) }
+            let(:storage) { create(:share_point_dev_storage, oauth_client_token_user: user) }
 
             let(:auth_strategy) { Registry["share_point.authentication.userless"].call(false) }
             let(:input_data) { Input::Files.build(folder:).value! }
 
             it_behaves_like "adapter files_query: basic query setup"
 
+            # rubocop:disable Layout/LineLength
             context "when parent folder is root, return a list of drives", vcr: "share_point/files_query_root" do
               let(:folder) { "/" }
               let(:files_result) do
                 Results::StorageFileCollection.new(
                   files: [
                     Results::StorageFile.new(
-                      id: "b!FeOZEMfQx0eGQKqVBLcP__BG8mq-4-9FuRqOyk3MXY8Qconfm2i6SKEoCmuGYqQK",
+                      id: "b!FeOZEMfQx0eGQKqVBLcP__BG8mq-4-9FuRqOyk3MXY8Qconfm2i6SKEoCmuGYqQK||",
                       name: "OpenProject",
                       mime_type: "application/x-op-drive",
-                      location: "/drives/b!FeOZEMfQx0eGQKqVBLcP__BG8mq-4-9FuRqOyk3MXY8Qconfm2i6SKEoCmuGYqQK",
+                      location: "/OPTest/OpenProject",
                       permissions: %i[readable writeable]
                     ),
                     Results::StorageFile.new(
-                      id: "b!FeOZEMfQx0eGQKqVBLcP__BG8mq-4-9FuRqOyk3MXY87vnZ6fgfvQanZHX-XCAyw",
+                      id: "b!FeOZEMfQx0eGQKqVBLcP__BG8mq-4-9FuRqOyk3MXY87vnZ6fgfvQanZHX-XCAyw||",
                       name: "Shared Documents",
                       mime_type: "application/x-op-drive",
-                      location: "/drives/b!FeOZEMfQx0eGQKqVBLcP__BG8mq-4-9FuRqOyk3MXY87vnZ6fgfvQanZHX-XCAyw",
+                      location: "/OPTest/Shared%20Documents",
                       permissions: %i[readable writeable]
                     ),
                     Results::StorageFile.new(
-                      id: "b!FeOZEMfQx0eGQKqVBLcP__BG8mq-4-9FuRqOyk3MXY8Pmdpc8mQ1QJkyIbbWQJol",
+                      id: "b!FeOZEMfQx0eGQKqVBLcP__BG8mq-4-9FuRqOyk3MXY8Pmdpc8mQ1QJkyIbbWQJol||",
                       name: "Selected Permissions",
                       mime_type: "application/x-op-drive",
-                      location: "/drives/b!FeOZEMfQx0eGQKqVBLcP__BG8mq-4-9FuRqOyk3MXY8Pmdpc8mQ1QJkyIbbWQJol",
+                      location: "/OPTest/Selected%20Permissions",
                       permissions: %i[readable writeable]
                     ),
                     Results::StorageFile.new(
-                      id: "b!FeOZEMfQx0eGQKqVBLcP__BG8mq-4-9FuRqOyk3MXY_YoKf1JPvYSJeFRsyx4zF_",
+                      id: "b!FeOZEMfQx0eGQKqVBLcP__BG8mq-4-9FuRqOyk3MXY_YoKf1JPvYSJeFRsyx4zF_||",
                       name: "Chris document library",
                       mime_type: "application/x-op-drive",
-                      location: "/drives/b!FeOZEMfQx0eGQKqVBLcP__BG8mq-4-9FuRqOyk3MXY_YoKf1JPvYSJeFRsyx4zF_",
+                      location: "/OPTest/Chris%20document%20library",
                       permissions: %i[readable writeable]
                     ),
                     Results::StorageFile.new(
-                      id: "b!FeOZEMfQx0eGQKqVBLcP__BG8mq-4-9FuRqOyk3MXY8CfNaHr_0ERYs5kgmEWFrX",
+                      id: "b!FeOZEMfQx0eGQKqVBLcP__BG8mq-4-9FuRqOyk3MXY8CfNaHr_0ERYs5kgmEWFrX||",
                       name: "Marcello AMPF",
                       mime_type: "application/x-op-drive",
-                      location: "/drives/b!FeOZEMfQx0eGQKqVBLcP__BG8mq-4-9FuRqOyk3MXY8CfNaHr_0ERYs5kgmEWFrX",
+                      location: "/OPTest/Marcello%20AMPF",
                       permissions: %i[readable writeable]
                     ),
                     Results::StorageFile.new(
-                      id: "b!FeOZEMfQx0eGQKqVBLcP__BG8mq-4-9FuRqOyk3MXY8opHtYeMANTahXlS54FgHn",
+                      id: "b!FeOZEMfQx0eGQKqVBLcP__BG8mq-4-9FuRqOyk3MXY8opHtYeMANTahXlS54FgHn||",
                       name: "Dominic",
                       mime_type: "application/x-op-drive",
-                      location: "/drives/b!FeOZEMfQx0eGQKqVBLcP__BG8mq-4-9FuRqOyk3MXY8opHtYeMANTahXlS54FgHn",
+                      location: "/OPTest/Dominic",
                       permissions: %i[readable writeable]
                     ),
                     Results::StorageFile.new(
-                      id: "b!FeOZEMfQx0eGQKqVBLcP__BG8mq-4-9FuRqOyk3MXY93AQ5rgPKoR7tMwpspgj95",
+                      id: "b!FeOZEMfQx0eGQKqVBLcP__BG8mq-4-9FuRqOyk3MXY93AQ5rgPKoR7tMwpspgj95||",
                       name: "Markus",
                       mime_type: "application/x-op-drive",
-                      location: "/drives/b!FeOZEMfQx0eGQKqVBLcP__BG8mq-4-9FuRqOyk3MXY93AQ5rgPKoR7tMwpspgj95",
+                      location: "/OPTest/Markus",
                       permissions: %i[readable writeable]
                     ),
                     Results::StorageFile.new(
-                      id: "b!FeOZEMfQx0eGQKqVBLcP__BG8mq-4-9FuRqOyk3MXY9jo6leJDqrT7muzvmiWjFW",
+                      id: "b!FeOZEMfQx0eGQKqVBLcP__BG8mq-4-9FuRqOyk3MXY9jo6leJDqrT7muzvmiWjFW||",
                       name: "Marcello VCR",
                       mime_type: "application/x-op-drive",
-                      location: "/drives/b!FeOZEMfQx0eGQKqVBLcP__BG8mq-4-9FuRqOyk3MXY9jo6leJDqrT7muzvmiWjFW",
+                      location: "/OPTest/Marcello%20VCR",
                       permissions: %i[readable writeable]
                     )
                   ],
                   parent: Results::StorageFile.new(id: "1269877d26360587caf07834bc72ee3ad3c3698f1651bf85d8562e7fda19aa0f",
                                                    name: "OPTest",
-                                                   location: "/",
+                                                   location: "/OPTest",
                                                    permissions: %i[readable writeable]),
                   ancestors: []
                 )
@@ -117,6 +118,51 @@ module Storages
 
               it_behaves_like "adapter files_query: successful files response"
             end
+
+            context "when requesting a drive, return the list of all folders", vcr: "share_point/files_query_drive" do
+              let(:folder) { "b!FeOZEMfQx0eGQKqVBLcP__BG8mq-4-9FuRqOyk3MXY9jo6leJDqrT7muzvmiWjFW||" }
+              let(:files_result) do
+                Results::StorageFileCollection.new(
+                  files: [
+                    Results::StorageFile.new(
+                      id: "b!FeOZEMfQx0eGQKqVBLcP__BG8mq-4-9FuRqOyk3MXY9jo6leJDqrT7muzvmiWjFW||01ANJ53W5P3SUY3ZCDTRA3KLXRGA5A2M3S",
+                      name: "data",
+                      size: 0,
+                      mime_type: "application/x-op-directory",
+                      created_at: Time.zone.parse("2025-04-07 12:02:26Z"),
+                      last_modified_at: Time.zone.parse("2025-04-07 12:02:26Z"),
+                      created_by_name: "Eric Schubert",
+                      last_modified_by_name: "Eric Schubert",
+                      location: "/OPTest/Marcello%20VCR/data",
+                      permissions: %i[readable writeable]
+                    ),
+                    Results::StorageFile.new(
+                      id: "b!FeOZEMfQx0eGQKqVBLcP__BG8mq-4-9FuRqOyk3MXY9jo6leJDqrT7muzvmiWjFW||01ANJ53WZVLAWJSVFKOFF3HLYZPMPUK6HI",
+                      name: "simply_oidc.jpg",
+                      size: 56483,
+                      mime_type: "image/jpeg",
+                      created_at: Time.zone.parse("2025-04-07 12:02:42Z"),
+                      last_modified_at: Time.zone.parse("2025-04-07 12:02:42Z"),
+                      created_by_name: "Eric Schubert",
+                      last_modified_by_name: "Eric Schubert",
+                      location: "/OPTest/Marcello%20VCR/simply_oidc.jpg",
+                      permissions: %i[readable writeable]
+                    )
+                  ],
+                  parent: Results::StorageFile.new(
+                    id: "b!FeOZEMfQx0eGQKqVBLcP__BG8mq-4-9FuRqOyk3MXY9jo6leJDqrT7muzvmiWjFW||",
+                    name: "Marcello VCR",
+                    mime_type: "application/x-op-drive",
+                    location: "/OPTest/Marcello%20VCR",
+                    permissions: %i[readable writeable]
+                  ),
+                  ancestors: []
+                )
+              end
+
+              it_behaves_like "adapter files_query: successful files response"
+            end
+            # rubocop:enable Layout/LineLength
           end
         end
       end

franciscoxavier04/pm-BIM

modules/storages/spec/common/storages/adapters/providers/share_point/queries/files_query_spec.rb
@@ -38,72 +38,73 @@ module Storages
         module Queries
           RSpec.describe FilesQuery, :webmock do
             let(:user) { create(:admin) }
-            let(:storage) { create(:share_point_storage, oauth_client_token_user: user) }
+            let(:storage) { create(:share_point_dev_storage, oauth_client_token_user: user) }
 
             let(:auth_strategy) { Registry["share_point.authentication.userless"].call(false) }
             let(:input_data) { Input::Files.build(folder:).value! }
 
             it_behaves_like "adapter files_query: basic query setup"
 
-            context "when parent folder is root, return a list of drives", vcr: "share_point/files_query_root" do
+            # rubocop:disable Layout/LineLength
+            context "when parent folder is root", vcr: "share_point/files_query_root" do
               let(:folder) { "/" }
               let(:files_result) do
                 Results::StorageFileCollection.new(
                   files: [
                     Results::StorageFile.new(
-                      id: "b!FeOZEMfQx0eGQKqVBLcP__BG8mq-4-9FuRqOyk3MXY8Qconfm2i6SKEoCmuGYqQK",
+                      id: "b!FeOZEMfQx0eGQKqVBLcP__BG8mq-4-9FuRqOyk3MXY8Qconfm2i6SKEoCmuGYqQK||",
                       name: "OpenProject",
                       mime_type: "application/x-op-drive",
-                      location: "/drives/b!FeOZEMfQx0eGQKqVBLcP__BG8mq-4-9FuRqOyk3MXY8Qconfm2i6SKEoCmuGYqQK",
+                      location: "/OpenProject",
                       permissions: %i[readable writeable]
                     ),
                     Results::StorageFile.new(
-                      id: "b!FeOZEMfQx0eGQKqVBLcP__BG8mq-4-9FuRqOyk3MXY87vnZ6fgfvQanZHX-XCAyw",
+                      id: "b!FeOZEMfQx0eGQKqVBLcP__BG8mq-4-9FuRqOyk3MXY87vnZ6fgfvQanZHX-XCAyw||",
                       name: "Shared Documents",
                       mime_type: "application/x-op-drive",
-                      location: "/drives/b!FeOZEMfQx0eGQKqVBLcP__BG8mq-4-9FuRqOyk3MXY87vnZ6fgfvQanZHX-XCAyw",
+                      location: "/Shared%20Documents",
                       permissions: %i[readable writeable]
                     ),
                     Results::StorageFile.new(
-                      id: "b!FeOZEMfQx0eGQKqVBLcP__BG8mq-4-9FuRqOyk3MXY8Pmdpc8mQ1QJkyIbbWQJol",
+                      id: "b!FeOZEMfQx0eGQKqVBLcP__BG8mq-4-9FuRqOyk3MXY8Pmdpc8mQ1QJkyIbbWQJol||",
                       name: "Selected Permissions",
                       mime_type: "application/x-op-drive",
-                      location: "/drives/b!FeOZEMfQx0eGQKqVBLcP__BG8mq-4-9FuRqOyk3MXY8Pmdpc8mQ1QJkyIbbWQJol",
+                      location: "/Selected%20Permissions",
                       permissions: %i[readable writeable]
                     ),
                     Results::StorageFile.new(
-                      id: "b!FeOZEMfQx0eGQKqVBLcP__BG8mq-4-9FuRqOyk3MXY_YoKf1JPvYSJeFRsyx4zF_",
+                      id: "b!FeOZEMfQx0eGQKqVBLcP__BG8mq-4-9FuRqOyk3MXY_YoKf1JPvYSJeFRsyx4zF_||",
                       name: "Chris document library",
                       mime_type: "application/x-op-drive",
-                      location: "/drives/b!FeOZEMfQx0eGQKqVBLcP__BG8mq-4-9FuRqOyk3MXY_YoKf1JPvYSJeFRsyx4zF_",
+                      location: "/Chris%20document%20library",
                       permissions: %i[readable writeable]
                     ),
                     Results::StorageFile.new(
-                      id: "b!FeOZEMfQx0eGQKqVBLcP__BG8mq-4-9FuRqOyk3MXY8CfNaHr_0ERYs5kgmEWFrX",
+                      id: "b!FeOZEMfQx0eGQKqVBLcP__BG8mq-4-9FuRqOyk3MXY8CfNaHr_0ERYs5kgmEWFrX||",
                       name: "Marcello AMPF",
                       mime_type: "application/x-op-drive",
-                      location: "/drives/b!FeOZEMfQx0eGQKqVBLcP__BG8mq-4-9FuRqOyk3MXY8CfNaHr_0ERYs5kgmEWFrX",
+                      location: "/Marcello%20AMPF",
                       permissions: %i[readable writeable]
                     ),
                     Results::StorageFile.new(
-                      id: "b!FeOZEMfQx0eGQKqVBLcP__BG8mq-4-9FuRqOyk3MXY8opHtYeMANTahXlS54FgHn",
+                      id: "b!FeOZEMfQx0eGQKqVBLcP__BG8mq-4-9FuRqOyk3MXY8opHtYeMANTahXlS54FgHn||",
                       name: "Dominic",
                       mime_type: "application/x-op-drive",
-                      location: "/drives/b!FeOZEMfQx0eGQKqVBLcP__BG8mq-4-9FuRqOyk3MXY8opHtYeMANTahXlS54FgHn",
+                      location: "/Dominic",
                       permissions: %i[readable writeable]
                     ),
                     Results::StorageFile.new(
-                      id: "b!FeOZEMfQx0eGQKqVBLcP__BG8mq-4-9FuRqOyk3MXY93AQ5rgPKoR7tMwpspgj95",
+                      id: "b!FeOZEMfQx0eGQKqVBLcP__BG8mq-4-9FuRqOyk3MXY93AQ5rgPKoR7tMwpspgj95||",
                       name: "Markus",
                       mime_type: "application/x-op-drive",
-                      location: "/drives/b!FeOZEMfQx0eGQKqVBLcP__BG8mq-4-9FuRqOyk3MXY93AQ5rgPKoR7tMwpspgj95",
+                      location: "/Markus",
                       permissions: %i[readable writeable]
                     ),
                     Results::StorageFile.new(
-                      id: "b!FeOZEMfQx0eGQKqVBLcP__BG8mq-4-9FuRqOyk3MXY9jo6leJDqrT7muzvmiWjFW",
+                      id: "b!FeOZEMfQx0eGQKqVBLcP__BG8mq-4-9FuRqOyk3MXY9jo6leJDqrT7muzvmiWjFW||",
                       name: "Marcello VCR",
                       mime_type: "application/x-op-drive",
-                      location: "/drives/b!FeOZEMfQx0eGQKqVBLcP__BG8mq-4-9FuRqOyk3MXY9jo6leJDqrT7muzvmiWjFW",
+                      location: "/Marcello%20VCR",
                       permissions: %i[readable writeable]
                     )
                   ],
@@ -117,6 +118,128 @@ module Storages
 
               it_behaves_like "adapter files_query: successful files response"
             end
+
+            context "when requesting a drive", vcr: "share_point/files_query_drive" do
+              let(:folder) { "b!FeOZEMfQx0eGQKqVBLcP__BG8mq-4-9FuRqOyk3MXY9jo6leJDqrT7muzvmiWjFW||" }
+              let(:files_result) do
+                Results::StorageFileCollection.new(
+                  files: [
+                    Results::StorageFile.new(
+                      id: "b!FeOZEMfQx0eGQKqVBLcP__BG8mq-4-9FuRqOyk3MXY9jo6leJDqrT7muzvmiWjFW||01ANJ53W5P3SUY3ZCDTRA3KLXRGA5A2M3S",
+                      name: "data",
+                      size: 760,
+                      mime_type: "application/x-op-directory",
+                      created_at: Time.zone.parse("2025-04-07 12:02:26Z"),
+                      last_modified_at: Time.zone.parse("2025-04-07 12:02:26Z"),
+                      created_by_name: "Eric Schubert",
+                      last_modified_by_name: "Eric Schubert",
+                      location: "/Marcello%20VCR/data",
+                      permissions: %i[readable writeable]
+                    ),
+                    Results::StorageFile.new(
+                      id: "b!FeOZEMfQx0eGQKqVBLcP__BG8mq-4-9FuRqOyk3MXY9jo6leJDqrT7muzvmiWjFW||01ANJ53W2MWJ6SKEZPHFGIAAB325KYYMPE",
+                      name: "empty",
+                      size: 0,
+                      mime_type: "application/x-op-directory",
+                      created_at: Time.zone.parse("2025-07-28 08:46:36Z"),
+                      last_modified_at: Time.zone.parse("2025-07-28 08:46:36Z"),
+                      created_by_name: "Eric Schubert",
+                      last_modified_by_name: "Eric Schubert",
+                      location: "/Marcello%20VCR/empty",
+                      permissions: %i[readable writeable]
+                    ),
+                    Results::StorageFile.new(
+                      id: "b!FeOZEMfQx0eGQKqVBLcP__BG8mq-4-9FuRqOyk3MXY9jo6leJDqrT7muzvmiWjFW||01ANJ53WZVLAWJSVFKOFF3HLYZPMPUK6HI",
+                      name: "simply_oidc.jpg",
+                      size: 56483,
+                      mime_type: "image/jpeg",
+                      created_at: Time.zone.parse("2025-04-07 12:02:42Z"),
+                      last_modified_at: Time.zone.parse("2025-04-07 12:02:42Z"),
+                      created_by_name: "Eric Schubert",
+                      last_modified_by_name: "Eric Schubert",
+                      location: "/Marcello%20VCR/simply_oidc.jpg",
+                      permissions: %i[readable writeable]
+                    )
+                  ],
+                  parent: Results::StorageFile.new(
+                    id: "b!FeOZEMfQx0eGQKqVBLcP__BG8mq-4-9FuRqOyk3MXY9jo6leJDqrT7muzvmiWjFW||",
+                    name: "Marcello VCR",
+                    location: "/Marcello%20VCR",
+                    permissions: %i[readable writeable]
+                  ),
+                  ancestors: [
+                    Results::StorageFile.new(
+                      id: "a1d45ff742d2175c095f0a7173f93fc3fc23664a953ceae6778fe15398818c2d",
+                      name: "OPTest",
+                      location: "/",
+                      permissions: %i[readable writeable]
+                    )
+                  ]
+                )
+              end
+
+              it_behaves_like "adapter files_query: successful files response"
+            end
+
+            context "when requesting an folder", vcr: "share_point/files_query_folder" do
+              let(:folder) do
+                "b!FeOZEMfQx0eGQKqVBLcP__BG8mq-4-9FuRqOyk3MXY9jo6leJDqrT7muzvmiWjFW||01ANJ53W5P3SUY3ZCDTRA3KLXRGA5A2M3S"
+              end
+
+              let(:files_result) do
+                Results::StorageFileCollection.new(
+                  files: [
+                    Results::StorageFile.new(
+                      id: "b!FeOZEMfQx0eGQKqVBLcP__BG8mq-4-9FuRqOyk3MXY9jo6leJDqrT7muzvmiWjFW||01ANJ53W26P5RNXU7V2JBKCVQQAGGTO46A",
+                      name: "edge one_drive_health_report_2025-07-22T16_03_25Z.txt",
+                      size: 760,
+                      mime_type: "text/plain",
+                      location: "/Marcello%20VCR/data/edge%20one_drive_health_report_2025-07-22T16_03_25Z.txt",
+                      created_at: Time.zone.parse("2025-07-28 08:45:30.000000000 UTC +00:00"),
+                      last_modified_at: Time.zone.parse("2025-07-28 08:45:30.000000000 UTC +00:00"),
+                      created_by_name: "Eric Schubert",
+                      last_modified_by_name: "Eric Schubert",
+                      permissions: %i[readable writeable]
+                    )
+                  ],
+                  parent: Results::StorageFile.new(
+                    id: "b!FeOZEMfQx0eGQKqVBLcP__BG8mq-4-9FuRqOyk3MXY9jo6leJDqrT7muzvmiWjFW||01ANJ53W5P3SUY3ZCDTRA3KLXRGA5A2M3S",
+                    name: "data",
+                    location: "/Marcello%20VCR/data",
+                    permissions: %i[readable writeable]
+                  ),
+                  ancestors: [
+                    Results::StorageFile.new(
+                      id: "b!FeOZEMfQx0eGQKqVBLcP__BG8mq-4-9FuRqOyk3MXY9jo6leJDqrT7muzvmiWjFW||",
+                      name: "Marcello VCR",
+                      location: "/Marcello%20VCR",
+                      permissions: %i[readable writeable]
+                    ),
+                    Results::StorageFile.new(
+                      id: "a1d45ff742d2175c095f0a7173f93fc3fc23664a953ceae6778fe15398818c2d",
+                      name: "OPTest",
+                      location: "/",
+                      permissions: %i[readable writeable]
+                    )
+                  ]
+                )
+              end
+
+              it_behaves_like "adapter files_query: successful files response"
+            end
+
+            context "when requesting an unknown file", vcr: "share_point/files_query_file_not_found" do
+              let(:folder) { "b!FeOZEMfQx0eGQKqVBLcP__BG8mq-4-9FuRqOyk3MXY9jo6leJDqrT7muzvmiWjFW||POTATO" }
+
+              it_behaves_like "adapter files_query: not found", Internal::ChildrenQuery
+            end
+
+            context "when requestion an unknown drive", vcr: "share_point/files_query_drive_not_found" do
+              let(:folder) { "b!FeOZEMfQx0eGQKqVBLcP__BG8mq-4-9FuRqOyk3MXY9jo6leJDqrT7muzvPoTaTO" }
+
+              it_behaves_like "adapter files_query: not found", Internal::ChildrenQuery
+            end
+            # rubocop:enable Layout/LineLength
           end
         end
       end