c96720334b8cd59e8ad29051ef2557b43ddf392a
Mir Bhatia <m.bhatia@openproject.com>
2025-08-04 16:35:40 +0200

    Use component in storages

--- modules/storages/app/components/storages/admin/side_panel/email_updates_switch_component.rb
+++ modules/storages/app/components/storages/admin/side_panel/email_updates_switch_component.rb
@@ -30,13 +30,13 @@
 
 module Storages
   module Admin
-    class SidePanel::HealthNotificationsComponent < ApplicationComponent
-      include ApplicationHelper
+    class SidePanel::EmailUpdatesSwitchComponent < ApplicationComponent
       include OpTurbo::Streamable
       include OpPrimer::ComponentHelpers
 
       def initialize(storage:)
         super
+
         @storage = storage
       end
 
@@ -44,31 +44,17 @@ module Storages
         @storage.automatic_management_enabled?
       end
 
-      private
-
-      def health_status_indicator
-        case @storage.health_status
-        when "healthy"
-          { scheme: :success, label: I18n.t("storages.health.label_healthy") }
-        when "unhealthy"
-          { scheme: :danger, label: I18n.t("storages.health.label_error") }
-        else
-          { scheme: :attention, label: I18n.t("storages.health.label_pending") }
+      def call
+        component_wrapper do
+          render OpPrimer::EmailUpdatesSwitchComponent.new(
+            toggle: @storage.health_notifications_should_be_sent?,
+            path: change_health_notifications_enabled_admin_settings_storage_path(@storage),
+            title: I18n.t("storages.health_email_notifications.title"),
+            enabled_description: I18n.t("storages.health_email_notifications.description_enabled"),
+            disabled_description: I18n.t("storages.health_email_notifications.description_disabled"),
+            method: :patch
+          )
         end
-      end
-
-      # This method returns the health identifier, description and the time since when the error occurs in a
-      # formatted manner. e.g. "Not found: Outbound request destination not found since 12/07/2023 03:45 PM"
-      def formatted_health_reason
-        identifier = @storage.health_reason_identifier.tr("_", " ").strip
-        description = @storage.health_reason_description
-
-        if description.present?
-          identifier.capitalize!
-          identifier << ": #{description}"
-        end
-
-        "#{identifier} #{I18n.t('storages.health.since', datetime: helpers.format_time(@storage.health_changed_at))}"
       end
     end
   end
